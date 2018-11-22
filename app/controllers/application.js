f831ff9b77143e2fd5e6025b609878d0504c140f
AbulAsar <abul.a@kiprosh.com>
2018-11-22 14:59:23 +0530

    Android deeplinking added

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,14 +1,25 @@
 import Ember from 'ember';
+import config from '../config/environment';
 
 export default Ember.Controller.extend({
 
   subscription: Ember.inject.controller(),
   cordova: Ember.inject.service(),
   store: Ember.inject.service(),
+  isMobileApp: config.cordova.enabled,
 
   initSubscription: Ember.on('init', function() {
     this.get('subscription').send('wire');
+    if (this.get('isMobileApp') && cordova.platformId === "android") { // jshint ignore:line
+      this.redirectToItem();
+    }
   }),
+
+  redirectToItem() {
+    universalLinks.subscribe("redirectToItem", (eventData) => { // jshint ignore:line
+      this.transitionToRoute(eventData.path);
+    });
+  },
 
   actions: {
     logMeOut() {
