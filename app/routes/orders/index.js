3f09a09c1ef8f119006cbf16050e85ac70e460bd
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2018-11-22 18:02:26 +0530

    CurrentUser loading issue fixed on Order's index page

--- app/routes/orders/index.js
+++ app/routes/orders/index.js
@@ -3,9 +3,11 @@ import AjaxPromise from 'stock/utils/ajax-promise';
 
 export default AuthorizeRoute.extend({
   model() {
-    return new AjaxPromise("/auth/current_user_profile", "GET", this.session.get("authToken"))
-      .then(data => {
-        this.store.pushPayload(data);
-      });
+    if(!this.session.get("currentUser")) {
+      return new AjaxPromise("/auth/current_user_profile", "GET", this.session.get("authToken"))
+        .then(data => {
+          this.store.pushPayload(data);
+        });
+    }
   }
 });
