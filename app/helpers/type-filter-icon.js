62e64f522187d1738f2e3aa1046fb7d7e0921b4e
Abhinav Garg <abhinav.g@kiprosh.com>
2018-12-20 18:04:03 +0530

    Upgrade to fa5

--- app/helpers/type-filter-icon.js
+++ app/helpers/type-filter-icon.js
@@ -3,22 +3,22 @@ import Ember from "ember";
 export default Ember.Helper.helper(function(type) {
   switch (type[0]) {
     case "in_stock":
-      return "fa fa-th-list";
+      return "th-list";
     case "designated":
-      return "fa fa-shopping-basket";
+      return "shopping-basket";
     case "online_orders":
       return "";
     case "appointment":
-      return "fa fa-handshake-o";
+      return "handshake";
     case "online_orders_pickup":
-      return "fa fa-male";
+      return "male";
     case "online_orders_ggv":
-      return "fa fa-truck";
+      return "truck";
     case "dispatched":
     case "shipments":
-      return "fa fa-ship";
+      return "ship";
     case "other":
-      return "fa fa-list-alt";
+      return "list-alt";
     default:
       return "";
   }
