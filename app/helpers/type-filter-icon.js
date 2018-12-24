3be5e2d7ae718059ee6f5d4a973afac3d4b0b3d7
Abhinav Garg <abhinav.g@kiprosh.com>
2018-12-24 14:20:42 +0530

    fix broken icons

--- app/helpers/type-filter-icon.js
+++ app/helpers/type-filter-icon.js
@@ -1,7 +1,7 @@
 import Ember from "ember";
 
 export default Ember.Helper.helper(function(type) {
-  switch (type[0]) {
+switch (type[0]) {
     case "in_stock":
       return "th-list";
     case "designated":
