ed4e197ba9c668e7a03ab82a18f1892f1808690d
Abhinav Garg <abhinav.g@kiprosh.com>
2019-01-21 19:39:30 +0530

    modify routes

--- app/controllers/orders/conversation.js
+++ app/controllers/orders/conversation.js
@@ -1,40 +1,37 @@
 import detail from "./detail";
 import Ember from "ember";
-import _ from 'lodash';
-const {  getOwner } = Ember;
 
 export default detail.extend({
   body: '',
-  ordersController: Ember.inject.controller('orders/index'),
-  order: Ember.computed.alias("ordersController.model"),
+  isPrivate: false,
 
-  createMessage(values) {
-    debugger
-    var message = this.store.createRecord("message", values);
-    message.save()
-      .then(() => {
-        this.set("body", "");
-      })
-      .catch(error => {
-        this.store.unloadRecord(message);
-        throw error;
-      });
-  },
-
-  allMessages: Ember.computed(function () {
-    return this.store.peekAll("message");
+  messages: Ember.computed("model", function(){
+    return this.get('model.messages');
   }),
 
-  messages: Ember.computed("allMessages.[]", "order", function () {
-    var messages = this.get("allMessages");
-    messages.filterBy("order.id", this.get("item.id"));
-    return messages.filterBy("isPrivate", this.get("isPrivate"));
+  createMessage(values) {
+    var message = this.store.createRecord("message", values);
+    message.save()
+    .then(() => {
+      this.set("body", "");
+    })
+    .catch(error => {
+      this.store.unloadRecord(message);
+      throw error;
+    });
+  },
+
+  ordersPackagesLengthMoreThenThree: Ember.observer('model', function () {
+    console.log('Okay okay');
   }),
 
   actions: {
     sendMessage(){
-      var values = this.getProperties("body", "order");
+      Ember.$("textarea").trigger('blur');
+      var values = this.getProperties("body");
+      values.designation = this.get('model.designation');
       values.createdAt = new Date();
+      values.isPrivate = this.get('isPrivate');
       values.sender = this.store.peekRecord("user", this.get("session.currentUser.id"));
       this.createMessage(values);
     }
