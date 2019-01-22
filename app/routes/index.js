199f99b6490f49ab5fa634b0fded1a3de9872afb
Patrick R <patrixr@Patricks-MacBook-Pro.local>
2019-01-22 14:27:47 +0800

    Fix dashboard counters for supervisor

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,7 @@ import AjaxPromise from 'stock/utils/ajax-promise';
 
 export default SessionRoute.extend({
   model() {
-    let isOrderFulfilmentRole = this.get('session.currentUser.isOrderFulfilmentUser');
+    let canViewDashboard = this.get('session.currentUser.canViewDashboard');
 
     var recentlyUsedDesignations = this.get('store').query('designation', { shallow: true, recently_used: true });
     var recentlyUsedLocations = this.get('store').query('location', { recently_used: true });
@@ -17,7 +17,7 @@ export default SessionRoute.extend({
     this.get('store').pushPayload(recentlyUsedDesignations);
     this.get('store').pushPayload(recentlyUsedLocations);
 
-    if (isOrderFulfilmentRole) {
+    if (canViewDashboard) {
       return Ember.RSVP.hash({
         ordersCount: new AjaxPromise("/orders/summary", "GET", this.session.get("authToken")),
         recentlyUsedDesignations: recentlyUsedDesignations
