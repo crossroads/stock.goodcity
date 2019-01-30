fc4dbacf377f336f806c2ad872ce900417cbbcb3
AbulAsar <abul.a@kiprosh.com>
2019-01-31 01:09:19 +0530

    user_name added for recent_orders

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,21 +1,27 @@
-import Ember from 'ember';
+import Ember from 'ember';/* jshint ignore */
 import SessionRoute from './session';
-import AjaxPromise from 'stock/utils/ajax-promise';
+import AjaxPromise from 'stock/utils/ajax-promise';/* jshint ignore*/
 
 export default SessionRoute.extend({
-  model() {
+  /* jshint ignore:start */
+  async model() {
     let canViewDashboard = this.get('session.currentUser.canViewDashboard');
+    let store = this.store;
 
-    var recentlyUsedDesignations = this.get('store').query('designation', { shallow: true, recently_used: true });
-    var recentlyUsedLocations = this.get('store').query('location', { recently_used: true });
+    var recentlyUsedDesignations = await store.query('designation', { shallow: true, recently_used: true });
+    var recentlyUsedLocations = store.query('location', { recently_used: true });
     recentlyUsedDesignations.forEach(record => {
-        if(record.constructor.toString() === "stock@model:designation:") {
-          this.store.query("orders_package", { search_by_order_id: record.get("id")
-        });
-        }
-      });
-    this.get('store').pushPayload(recentlyUsedDesignations);
-    this.get('store').pushPayload(recentlyUsedLocations);
+      if(record.constructor.toString() === "stock@model:designation:") {
+        store.query("orders_package", { search_by_order_id: record.get("id")});
+      }
+    });
+    recentlyUsedDesignations.forEach(record => {
+      //let createdByUser =  store.peekRecord("user", record.get('createdById')) || store.findRecord("user", record.get('createdById'), { reload: false });
+      let createdByUser =  store.findRecord("user", record.get('createdById'), { reload: false });
+      store.pushPayload(createdByUser);
+    });
+    store.pushPayload(recentlyUsedDesignations);
+    store.pushPayload(recentlyUsedLocations);
 
     if (canViewDashboard) {
       return Ember.RSVP.hash({
@@ -24,4 +30,5 @@ export default SessionRoute.extend({
       });
     }
   }
+  /* jshint ignore:end */
 });
