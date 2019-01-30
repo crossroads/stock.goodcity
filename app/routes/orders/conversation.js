98388703f7ba42cf9b4f9b36b028505255d44823
Abhinav Garg <abhinav.g@kiprosh.com>
2019-01-31 00:10:50 +0530

    fix jshint errors

--- app/routes/orders/conversation.js
+++ app/routes/orders/conversation.js
@@ -4,19 +4,18 @@ import Ember from 'ember';
 export default detail.extend({
   model(params){
     var designation = this.store.peekRecord("designation", params.order_id, {
-      reload: true }) || this.store.findRecord('designation', params.order_id)
+      reload: true }) || this.store.findRecord('designation', params.order_id);
     return Ember.RSVP.hash({
       designation: designation,
       messages: this.store.query('message', { order_id: params.order_id })
     });
   },
 
-  afterModel(model) {
+  afterModel(model) { //jshint ignore:line
     //Overriding to neglect afterModel in detail
   },
 
   setupController(controller, model) {
-    // controller.set("messages", model.messages);
-    controller.set("model", model.designation)
+    controller.set("model", model.designation);
   }
 });
