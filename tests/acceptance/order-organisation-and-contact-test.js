eec1f00409c3c717945f65f419b97a2db0c37356
AbulAsar <abul.a@kiprosh.com>
2019-03-07 21:49:37 +0530

    tests cases added for summary client/beneficiary tab

--- tests/acceptance/order-organisation-and-contact-test.js
+++ tests/acceptance/order-organisation-and-contact-test.js
@@ -13,7 +13,7 @@ import '../factories/organisations_user';
 import FactoryGuy from 'ember-data-factory-guy';
 import { mockFindAll } from 'ember-data-factory-guy';
 
-var App, designation, item1, orders_package1, gc_organisation, beneficiary, identity_type, user, organisation_user, bookingType, data;
+var App, designation, item1, orders_package1, gc_organisation, beneficiary, identity_type, user, organisation_user, bookingType, data, request;
 
 module('Acceptance: Order summary', {
   beforeEach: function() {
@@ -25,17 +25,21 @@ module('Acceptance: Order summary', {
     identity_type = FactoryGuy.make('identity_type');
     beneficiary = FactoryGuy.make('beneficiary', { identity_type: identity_type });
     organisation_user = FactoryGuy.make("organisationsUser", { gcOrganisation: gc_organisation, user: user });
-    designation = FactoryGuy.make("designation", { state: "submitted", detailType: "GoodCity", beneficiary: beneficiary, gcOrganisation: gc_organisation, createdBy: user });
+    designation = FactoryGuy.make("designation", { state: "submitted", detailType: "GoodCity", goodcityRequests: request, gcOrganisation: gc_organisation, beneficiaryId: beneficiary.id, createdBy: user, userCancelledCount: 21, userAwaitingDispatchCount: 34, userSubmittedCount: 23, peopleHelped: '2', purposeDescription: 'Test' });
+    var designation1 = FactoryGuy.make("designation", { id: designation.id, state: "submitted", beneficiary: null, detailType: "GoodCity", goodcityRequests: request, gcOrganisation: gc_organisation, beneficiaryId: beneficiary.id, createdBy: user, userCancelledCount: 21, userAwaitingDispatchCount: 34, userSubmittedCount: 23, peopleHelped: '2', purposeDescription: 'Test' });
+    request = FactoryGuy.make("goodcity_request", { quantity: 1, designation: designation});
     item1 = FactoryGuy.make("item", { state: "submitted", quantity: 0 , designation: designation});
     orders_package1 = FactoryGuy.make("orders_package", { state: "dispatched", quantity: 1, item: item1, designation: designation });
     data = {"user_profile": [{"id": 2,"first_name": "David", "last_name": "Dara51", "mobile": "61111111", "user_role_ids": [1]}], "users": [{"id": 2,"first_name": "David", "last_name": "Dara51", "mobile": "61111111"}], "roles": [{"id": 4, "name": "Supervisor"}], "user_roles": [{"id": 1, "user_id": 2, "role_id": 4}]};
-
+    var designation1 = FactoryGuy.make("designation", { id: designation.id, benificiary: '', state: "submitted", detailType: "GoodCity", goodcityRequests: request, gcOrganisation: gc_organisation, createdBy: user, userCancelledCount: 21, userAwaitingDispatchCount: 34, userSubmittedCount: 23, peopleHelped: '2', purposeDescription: 'Test' });
     $.mockjax({url:"/api/v1/auth/current_user_profil*",
       responseText: data });
+
     $.mockjax({url: "/api/v1/designations/*", type: 'GET', status: 200,responseText: {
       designations: [designation.toJSON({includeId: true})],
       orders_packages: [orders_package1.toJSON({includeId: true})]
     }});
+
     $.mockjax({url:"/api/v1/orders/summar*", responseText: {
       "submitted":14,
       "awaiting_dispatch":1,
@@ -45,6 +49,11 @@ module('Acceptance: Order summary', {
       "priority_dispatching":1,
       "priority_processing":2,
       "priority_awaiting_dispatch":1
+    }});
+
+    $.mockjax({url: "api/v1/beneficiar*"+ beneficiary.id ,type: 'DELETE',status: 200,responseText: {}});
+    $.mockjax({url: '/api/v1/orders*', type: 'PUT', status: 200,responseText: {
+     designations: []
     }});
 
     visit("/");
@@ -69,11 +78,53 @@ module('Acceptance: Order summary', {
 });
 
 test("Order summary detail", function(assert) {
-  assert.expect(6);
+  assert.expect(9);
   assert.equal(currentPath(), "orders.contact_summary");
   assert.equal($('.organisation_name').text().trim(), gc_organisation.get("nameEn"));
   assert.equal($('#contact_name').text().trim(), designation.get("createdBy.fullName"));
   assert.equal($('#contact_mobile').text().trim(), designation.get("createdBy.mobile"));
   assert.equal($('#contact_position').text().trim(), designation.get("createdBy.position"));
   assert.equal($('#contact_email').text().trim(), designation.get("createdBy.email"));
+  assert.equal($('#submitted_count').text().trim(), designation.get("userSubmittedCount"));
+  assert.equal($('#scheduled_count').text().trim(), designation.get("userAwaitingDispatchCount"));
+  assert.equal($('#cancelled_count').text().trim(), designation.get("userCancelledCount"));
 });
+
+test("Clicking Client/Purpose lands to Client Page", function(assert) {
+  assert.equal(currentPath(), "orders.contact_summary");
+  click($('#client_tab'));
+  andThen(function() {
+    assert.equal(currentPath(), "orders.client_summary");
+  });
+})
+
+test("Client/Purpose displays beneficiary details", function(assert) {
+  assert.expect(8);
+  assert.equal(currentPath(), "orders.contact_summary");
+  click($('#client_tab'));
+  andThen(function() {
+    assert.equal(currentPath(), "orders.client_summary");
+    assert.equal($('.beneficiary-title').val(), designation.get("beneficiary.fullNameWithoutTitle"));
+    assert.equal($('.beneficiary-phone').val(), designation.get("beneficiary.mobileWithoutCountryCode"));
+    assert.equal($('.beneficiary-identity-type').val(), designation.get("beneficiary.identityType.name"));
+    assert.equal($('.identity-number').val(), designation.get("beneficiary.identityNumber"));
+    assert.equal($('.people-helped').val(), designation.get("peopleHelped"));
+    assert.equal($('.order-description-textarea').val(), designation.get("purposeDescription"));
+  });
+})
+
+// test("Clicking 'Remove client/beneficiary will remove beneficiary from Order", function(assert) {
+//   assert.equal(currentPath(), "orders.contact_summary");
+//   click($('#client_tab'));
+//   andThen(function() {
+//     assert.equal(currentPath(), "orders.client_summary");
+//     click($('#delete-beneficiary'));
+//   });
+//   andThen(function() {
+//     click($('#btn2'));
+//   });
+//   andThen(function() {
+//     return pauseTest();
+//     assert.equal($('#add-beneficiary').text().trim(), 'Add client/summary');
+//   });
+// });
