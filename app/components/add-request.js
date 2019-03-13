f3a92b2e1f778c3298d0f59dbca00ee1c85ab37b
AbulAsar <abul.a@kiprosh.com>
2019-03-13 19:48:10 +0530

    Remove common code from for add-request from goods_details and orders request page into component

--- app/components/add-request.js
+++ app/components/add-request.js
@@ -3,7 +3,12 @@ const { getOwner } = Ember;
 import AjaxPromise from "stock/utils/ajax-promise";
 
 export default Ember.Component.extend({
+  layoutName: null,
+  isGCRequest: null,
+
   store: Ember.inject.service(),
+  messageBox: Ember.inject.service(),
+  i18n: Ember.inject.service(),
   request: null,
   num: null,
   order: null,
@@ -12,11 +17,19 @@ export default Ember.Component.extend({
     return this.get("request.code.name");
   }),
 
+  onInit: Ember.on("init", function() {
+    if (this.get("isGCRequest")) {
+      this.set("layoutName", "components/appointment-add-request");
+    } else {
+      this.set("layoutName", "components/order-add-request");
+    }
+  }),
+
   actions: {
     deleteRequest(reqId) {
       var i18n = this.get("i18n");
       this.get("messageBox").custom(
-        i18n.t("order.request.remove_req").string + this.get("model.code"),
+        i18n.t("order.request.remove_req").string + this.get("order.code"),
         i18n.t("order.request.remove").string,
         () => this.send("removeRequest", reqId),
         i18n.t("not_now").string
