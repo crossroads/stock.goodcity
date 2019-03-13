5d4a3f36ae93dd57262b05fa2c4d5d082b689637
AbulAsar <abul.a@kiprosh.com>
2019-03-13 15:01:23 +0530

    add computed property for request name

--- app/components/add-request.js
+++ app/components/add-request.js
@@ -1,6 +1,6 @@
 import Ember from "ember";
 const { getOwner } = Ember;
-import AjaxPromise from 'stock/utils/ajax-promise';
+import AjaxPromise from "stock/utils/ajax-promise";
 
 export default Ember.Component.extend({
   store: Ember.inject.service(),
@@ -8,8 +8,8 @@ export default Ember.Component.extend({
   num: null,
   order: null,
 
-  packageTypeName: Ember.computed('request.packageType.name', function(){
-    return this.get('request.code.name');
+  packageTypeName: Ember.computed("request.code.name", function() {
+    return this.get("request.code.name");
   }),
 
   actions: {
@@ -17,7 +17,8 @@ export default Ember.Component.extend({
       var i18n = this.get("i18n");
       this.get("messageBox").custom(
         i18n.t("order.request.remove_req").string + this.get("model.code"),
-        i18n.t("order.request.remove").string, () => this.send("removeRequest", reqId),
+        i18n.t("order.request.remove").string,
+        () => this.send("removeRequest", reqId),
         i18n.t("not_now").string
       );
     },
@@ -25,19 +26,23 @@ export default Ember.Component.extend({
     removeRequest(reqId) {
       var url = `/goodcity_requests/${reqId}`;
       var req = this.get("store").peekRecord("goodcity_request", reqId);
-      var loadingView = getOwner(this).lookup('component:loading').append();
-      new AjaxPromise(url, "DELETE", this.get('session.authToken'))
-      .then(data => {
-        this.get("store").pushPayload(data);
-      })
-      .finally(() => {
-        loadingView.destroy();
-        this.get("store").unloadRecord(req);
-      });
+      var loadingView = getOwner(this)
+        .lookup("component:loading")
+        .append();
+      new AjaxPromise(url, "DELETE", this.get("session.authToken"))
+        .then(data => {
+          this.get("store").pushPayload(data);
+        })
+        .finally(() => {
+          loadingView.destroy();
+          this.get("store").unloadRecord(req);
+        });
     },
 
-    searchPackageType(reqId, orderId){
-      this.get('router').transitionTo('order.search_code', orderId, { queryParams: { reqId: reqId }});
+    searchPackageType(reqId, orderId) {
+      this.get("router").transitionTo("order.search_code", orderId, {
+        queryParams: { reqId: reqId }
+      });
     },
 
     //Fix: Too deeply nested component(3 levels) failing randomly(Known issue with Ember)
