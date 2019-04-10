c727cd8bc507d1bf96edabbe4b007ea0698d6aea
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2019-04-10 09:25:37 +0530

    Revert "GCW:2413 Language switcher fix"
    This reverts commit 222a72479d40a0ce65b989b8b168558114c909a0.

--- app/components/language-switcher.js
+++ app/components/language-switcher.js
@@ -3,11 +3,11 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
   isEnglish: Ember.computed('session.language', function() {
-    return JSON.parse(window.localStorage.getItem("language")) === 'en';
+    return this.get('session.language') === 'en';
   }),
 
   isChinese: Ember.computed('session.language', function() {
-    return JSON.parse(window.localStorage.getItem("language")) === 'zh-tw';
+    return this.get('session.language') === 'zh-tw';
   }),
 
   actions: {
