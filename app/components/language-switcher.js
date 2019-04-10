222a72479d40a0ce65b989b8b168558114c909a0
Darshan Chauhan <darshan.chauhan@kiprosh.com>
2019-04-10 09:05:50 +0530

    GCW:2413 Language switcher fix

--- app/components/language-switcher.js
+++ app/components/language-switcher.js
@@ -3,11 +3,11 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
   isEnglish: Ember.computed('session.language', function() {
-    return this.get('session.language') === 'en';
+    return JSON.parse(window.localStorage.getItem("language")) === 'en';
   }),
 
   isChinese: Ember.computed('session.language', function() {
-    return this.get('session.language') === 'zh-tw';
+    return JSON.parse(window.localStorage.getItem("language")) === 'zh-tw';
   }),
 
   actions: {
