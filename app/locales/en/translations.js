2e6312005ec791d00dec25bd398ac8d706433d46
namrataukirde <namrata.ukirde@kiprosh.com>
2019-04-15 16:55:50 +0530

    add preffered_contact_number to orders summary tab

--- app/locales/en/translations.js
+++ app/locales/en/translations.js
@@ -82,6 +82,7 @@ export default {
     organisation_title: "Organisation",
     contact_name: "Name",
     contact_phone: "Phone",
+    preferred_contact_number: "Preferred contact number",
     contact_position: "Position",
     contact_email: "Email",
     contact_last_seen: "Last seen",
--- app/models/organisations_user.js
+++ app/models/organisations_user.js
@@ -1,12 +1,19 @@
-import attr from 'ember-data/attr';
-import Model from 'ember-data/model';
-import { belongsTo } from 'ember-data/relationships';
+import attr from "ember-data/attr";
+import Model from "ember-data/model";
+import { belongsTo } from "ember-data/relationships";
 
 export default Model.extend({
-  position: attr('string'),
-  organisationId: attr('number'),
+  position: attr("string"),
+  preferredContactNumber: attr("string"),
+  organisationId: attr("number"),
 
-  gcOrganisation: belongsTo('gcOrganisation', { async: false }),
-  organisation: belongsTo('organisation', { async: false }),
-  user: belongsTo('user', { async: false })
+  gcOrganisation: belongsTo("gcOrganisation", {
+    async: false
+  }),
+  organisation: belongsTo("organisation", {
+    async: false
+  }),
+  user: belongsTo("user", {
+    async: false
+  })
 });
