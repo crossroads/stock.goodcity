3524f87936cc16e3ec2392b7bf279a65fc73c4be
AbulAsar <abul.a@kiprosh.com>
2019-04-16 23:47:51 +0530

    ember-infinity version upgraded

--- app/routes/search_organisation.js
+++ app/routes/search_organisation.js
@@ -1,4 +1,13 @@
-import AuthorizeRoute from './authorize';
+import AuthorizeRoute from "./authorize";
 
 export default AuthorizeRoute.extend({
+  infinity: Ember.inject.service(),
+
+  model() {
+    return this.get("infinity").model("gcOrganisation", {
+      perPage: 12,
+      startingPage: 1,
+      searchText: "jockey"
+    });
+  }
 });
