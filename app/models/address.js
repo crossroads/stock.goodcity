47091f71d56f102fccd8c91ea33030f5784e237f
namrataukirde <namrata.ukirde@kiprosh.com>
2019-05-06 22:20:06 +0530

    make addressable on demand

--- app/models/address.js
+++ app/models/address.js
@@ -1,20 +1,25 @@
-import Model from 'ember-data/model';
-import attr from 'ember-data/attr';
-import { belongsTo } from 'ember-data/relationships';
-import Ember from 'ember';
+import Model from "ember-data/model";
+import attr from "ember-data/attr";
+import { belongsTo } from "ember-data/relationships";
+import Ember from "ember";
 
 export default Model.extend({
-  flat:        attr('string'),
-  building:    attr('string'),
-  street:      attr('string'),
-  addressType: attr('string'),
+  flat: attr("string"),
+  building: attr("string"),
+  street: attr("string"),
+  addressType: attr("string"),
 
-  district:    belongsTo('district', { async: false }),
+  district: belongsTo("district", {
+    async: false
+  }),
 
-  addressableType: attr('string'),
-  addressable: belongsTo('addressable', { polymorphic: true, async: false }),
+  addressableType: attr("string"),
+  addressable: belongsTo("addressable", {
+    polymorphic: true,
+    async: true
+  }),
 
-  fullAddress: Ember.computed('flat', 'building', 'street', function() {
-    return `${this.get('flat')} ${this.get('building')} ${this.get('street')}`;
+  fullAddress: Ember.computed("flat", "building", "street", function() {
+    return `${this.get("flat")} ${this.get("building")} ${this.get("street")}`;
   })
 });
