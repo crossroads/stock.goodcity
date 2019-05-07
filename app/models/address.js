9c63a63f5052dc2bd865ceecf08bd076c6f69e6d
namrataukirde <namrata.ukirde@kiprosh.com>
2019-05-07 09:28:56 +0530

    Revert "make addressable on demand"
    This reverts commit 47091f71d56f102fccd8c91ea33030f5784e237f.

--- app/models/address.js
+++ app/models/address.js
@@ -1,25 +1,20 @@
-import Model from "ember-data/model";
-import attr from "ember-data/attr";
-import { belongsTo } from "ember-data/relationships";
-import Ember from "ember";
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { belongsTo } from 'ember-data/relationships';
+import Ember from 'ember';
 
 export default Model.extend({
-  flat: attr("string"),
-  building: attr("string"),
-  street: attr("string"),
-  addressType: attr("string"),
+  flat:        attr('string'),
+  building:    attr('string'),
+  street:      attr('string'),
+  addressType: attr('string'),
 
-  district: belongsTo("district", {
-    async: false
-  }),
+  district:    belongsTo('district', { async: false }),
 
-  addressableType: attr("string"),
-  addressable: belongsTo("addressable", {
-    polymorphic: true,
-    async: true
-  }),
+  addressableType: attr('string'),
+  addressable: belongsTo('addressable', { polymorphic: true, async: false }),
 
-  fullAddress: Ember.computed("flat", "building", "street", function() {
-    return `${this.get("flat")} ${this.get("building")} ${this.get("street")}`;
+  fullAddress: Ember.computed('flat', 'building', 'street', function() {
+    return `${this.get('flat')} ${this.get('building')} ${this.get('street')}`;
   })
 });
