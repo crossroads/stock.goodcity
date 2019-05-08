04dfe5d5268419a978b13135cbce78e1eae080fa
Darshan Chauhan <darshan.chauhan@kiprosh.com>
2019-05-08 10:19:56 +0530

    Relationship name changed

--- app/models/orders_process_checklist.js
+++ app/models/orders_process_checklist.js
@@ -3,7 +3,7 @@ import attr from "ember-data/attr";
 import { belongsTo } from "ember-data/relationships";
 
 export default Model.extend({
-  designation: belongsTo("designation", {
+  order: belongsTo("designation", {
     async: false
   }),
   orderId: attr("number"),
