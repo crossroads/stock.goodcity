f5cabf99d6b3cfd789b2088e0539a7652a6e0352
namrataukirde <namrata.ukirde@kiprosh.com>
2019-05-08 13:03:14 +0530

    remove validation of process checklist to finish processing

--- app/controllers/orders/detail.js
+++ app/controllers/orders/detail.js
@@ -1,23 +1,22 @@
 import Ember from "ember";
-import config from '../../config/environment';
-import AjaxPromise from 'stock/utils/ajax-promise';
-import GoodcityController from '../goodcity_controller';
-import _ from 'lodash';
+import config from "../../config/environment";
+import AjaxPromise from "stock/utils/ajax-promise";
+import GoodcityController from "../goodcity_controller";
+import _ from "lodash";
 const { getOwner } = Ember;
 
 export default GoodcityController.extend({
-
   backLinkPath: "",
   displayAllItems: false,
   displayItemOptions: true,
   isMobileApp: config.cordova.enabled,
-  order: Ember.computed.alias('model'),
-  hasUnreadMessages: Ember.computed('order', function () {
-    return this.get('order.hasUnreadMessages');
+  order: Ember.computed.alias("model"),
+  hasUnreadMessages: Ember.computed("order", function() {
+    return this.get("order.hasUnreadMessages");
   }),
 
-  unreadMessagesCount: Ember.computed('order', function () {
-    return this.get('order.unreadMessagesCount');
+  unreadMessagesCount: Ember.computed("order", function() {
+    return this.get("order.unreadMessagesCount");
   }),
 
   itemIdforHistoryRoute: null,
@@ -52,32 +51,72 @@ export default GoodcityController.extend({
     }
   }),
 
-  ordersPackagesLengthMoreThenThree: Ember.observer('model.ordersPackages', function() {
-    var ordersPackages = this.get("model.ordersPackages");
-    ordersPackages.canonicalState.forEach(record => {
-      if(record && record._data.state === "cancelled") {
+  ordersPackagesLengthMoreThenThree: Ember.observer(
+    "model.ordersPackages",
+    function() {
+      var ordersPackages = this.get("model.ordersPackages");
+      ordersPackages.canonicalState.forEach(record => {
+        if (record && record._data.state === "cancelled") {
           ordersPackages.canonicalState.removeObject(record);
-      }
-    });
-    return (ordersPackages.canonicalState.length) >= 3 ? this.set("displayAllItems", false) : this.set("displayAllItems", true);
-  }),
+        }
+      });
+      return ordersPackages.canonicalState.length >= 3
+        ? this.set("displayAllItems", false)
+        : this.set("displayAllItems", true);
+    }
+  ),
 
-  itemsList: Ember.computed('model.items', 'displayAllItems', 'model.ordersPackages', 'model.ordersPackages.@each.quantity', 'model.ordersPackages.@each.state', function() {
-    var ordersPackages =  this.get("model.ordersPackages").rejectBy('state', "requested").rejectBy("state", "cancelled").rejectBy('state', null);
-    return this.get("displayAllItems") ? ordersPackages : ordersPackages.slice(0, 3);
-  }),
+  itemsList: Ember.computed(
+    "model.items",
+    "displayAllItems",
+    "model.ordersPackages",
+    "model.ordersPackages.@each.quantity",
+    "model.ordersPackages.@each.state",
+    function() {
+      var ordersPackages = this.get("model.ordersPackages")
+        .rejectBy("state", "requested")
+        .rejectBy("state", "cancelled")
+        .rejectBy("state", null);
+      return this.get("displayAllItems")
+        ? ordersPackages
+        : ordersPackages.slice(0, 3);
+    }
+  ),
 
-  canceledItemsList: Ember.computed('model.items', 'displayAllItems', 'model.ordersPackages', 'model.ordersPackages.@each.quantity', 'model.ordersPackages.@each.state', function() {
-    var ordersPackages =  this.get("model.ordersPackages").filterBy('state', "cancelled").rejectBy('state', null);
-    return this.get("displayAllItems") ? ordersPackages : ordersPackages.slice(0, 3);
-  }),
+  canceledItemsList: Ember.computed(
+    "model.items",
+    "displayAllItems",
+    "model.ordersPackages",
+    "model.ordersPackages.@each.quantity",
+    "model.ordersPackages.@each.state",
+    function() {
+      var ordersPackages = this.get("model.ordersPackages")
+        .filterBy("state", "cancelled")
+        .rejectBy("state", null);
+      return this.get("displayAllItems")
+        ? ordersPackages
+        : ordersPackages.slice(0, 3);
+    }
+  ),
 
-  ordersPkgLength: Ember.computed('model.items', 'displayAllItems', 'model.ordersPackages', 'model.ordersPackages.@each.quantity', 'model.ordersPackages.@each.state', function() {
-    return this.get("model.ordersPackages").rejectBy('state', 'requested').rejectBy('state', null).length;
-  }),
+  ordersPkgLength: Ember.computed(
+    "model.items",
+    "displayAllItems",
+    "model.ordersPackages",
+    "model.ordersPackages.@each.quantity",
+    "model.ordersPackages.@each.state",
+    function() {
+      return this.get("model.ordersPackages")
+        .rejectBy("state", "requested")
+        .rejectBy("state", null).length;
+    }
+  ),
 
   formatTimeSlot(hours, minutes) {
-    return moment().set('hour', hours).set('minute', minutes).format('hh:mmA');
+    return moment()
+      .set("hour", hours)
+      .set("minute", minutes)
+      .format("hh:mmA");
   },
 
   genericCustomPopUp(message, button1text, button2text, btn1Callback) {
@@ -85,67 +124,81 @@ export default GoodcityController.extend({
     _this.get("messageBox").custom(
       _this.get("i18n").t(message),
       _this.get("i18n").t(button1text),
-      () => { btn1Callback(); },
+      () => {
+        btn1Callback();
+      },
       _this.get("i18n").t(button2text),
-      () => { this.send("toggleDisplayOptions"); });
+      () => {
+        this.send("toggleDisplayOptions");
+      }
+    );
   },
 
   genericAlertPopUp(message, btn1Callback = _.noop) {
     var _this = this;
-    this.get("messageBox").alert(_this.get("i18n").t(message),
-      () => { btn1Callback(); }
-      );
+    this.get("messageBox").alert(_this.get("i18n").t(message), () => {
+      btn1Callback();
+    });
   },
 
   //Should only be able to close if at least 1 item is dispatched and 0 is designated
   canCloseOrder(order) {
     let ordersPackages = order.get("ordersPackages");
-    return (ordersPackages.filterBy('state', "dispatched").length > 0 && !ordersPackages.filterBy('state', 'designated').length);
+    return (
+      ordersPackages.filterBy("state", "dispatched").length > 0 &&
+      !ordersPackages.filterBy("state", "designated").length
+    );
   },
 
   //Should only be able to cancel if 0 items are dispatched
   canCancelOrder(order) {
-    return !order.get("ordersPackages").filterBy('state', 'dispatched').length;
+    return !order.get("ordersPackages").filterBy("state", "dispatched").length;
   },
 
   actions: {
     openSchedulePopup() {
-      const scheduledAt = this.get('model.orderTransport.scheduledAt');
+      const scheduledAt = this.get("model.orderTransport.scheduledAt");
       try {
         const d = new Date(scheduledAt);
         const timeString = this.formatTimeSlot(d.getHours(), d.getMinutes());
-        const currentTimeSlot = _.find(this.get('scheduleTimeSlots'), ['id', timeString]);
-        this.set('selectedTimeslot', currentTimeSlot || this.get('scheduleTimeSlots')[0]);
-        this.set('selectedScheduleDate', d);
-        this.set("placeHolderDate", moment(d).format('ddd MMM do'));
+        const currentTimeSlot = _.find(this.get("scheduleTimeSlots"), [
+          "id",
+          timeString
+        ]);
+        this.set(
+          "selectedTimeslot",
+          currentTimeSlot || this.get("scheduleTimeSlots")[0]
+        );
+        this.set("selectedScheduleDate", d);
+        this.set("placeHolderDate", moment(d).format("ddd MMM do"));
       } catch (e) {
-        this.set('selectedTimeslot', this.get('scheduleTimeSlots')[0]);
-        this.set('selectedScheduleDate', null);
+        this.set("selectedTimeslot", this.get("scheduleTimeSlots")[0]);
+        this.set("selectedScheduleDate", null);
       }
-      this.set('scheduleChangePopupVisible', true);
+      this.set("scheduleChangePopupVisible", true);
     },
 
     closeSchedulePopup() {
-      this.set('scheduleChangePopupVisible', false);
+      this.set("scheduleChangePopupVisible", false);
     },
 
     saveNewSchedule() {
-      let ts = this.get('selectedTimeslot');
-      let date = this.get('selectedScheduleDate');
+      let ts = this.get("selectedTimeslot");
+      let date = this.get("selectedScheduleDate");
       if (!date || !ts) {
-        return this.showError('Please select a valid date and timeslot');
+        return this.showError("Please select a valid date and timeslot");
       }
 
       date.setHours(ts.hours);
       date.setMinutes(ts.minutes);
-      this.updateRecord(this.get('model.orderTransport'), {
+      this.updateRecord(this.get("model.orderTransport"), {
         timeslot: ts.id,
         scheduledAt: date
       });
     },
 
     selectTimeslot(ts) {
-      this.set('selectedTimeslot', ts);
+      this.set("selectedTimeslot", ts);
     },
 
     toggleOrderOptions() {
@@ -157,7 +210,7 @@ export default GoodcityController.extend({
     },
 
     updateOrder(order, actionName) {
-      switch(actionName) {
+      switch (actionName) {
         case "messagePopUp":
           this.send("changeOrderState", order, "cancel");
           break;
@@ -165,7 +218,7 @@ export default GoodcityController.extend({
           this.send("changeOrderState", order, actionName);
           break;
         case "resubmit":
-          this.send("promptResubmitModel", order ,actionName);
+          this.send("promptResubmitModel", order, actionName);
           break;
         case "reopen":
           this.send("promptReopenModel", order, actionName);
@@ -183,11 +236,11 @@ export default GoodcityController.extend({
           this.send("promptCloseOrderModel", order, actionName);
           break;
         case "finish_processing":
-          if (this.get('processingChecklist').checklistCompleted(order)) {
-            this.send("changeOrderState", order, actionName);
-          } else {
-            this.genericAlertPopUp('order_details.logistics.checklist_incomplete');
-          }
+          // if (this.get('processingChecklist').checklistCompleted(order)) {
+          this.send("changeOrderState", order, actionName);
+          // } else {
+          //   this.genericAlertPopUp('order_details.logistics.checklist_incomplete');
+          // }
           break;
         default:
           this.send("changeOrderState", order, actionName);
@@ -196,62 +249,125 @@ export default GoodcityController.extend({
 
     dispatchLaterModel(order, actionName) {
       var _this = this;
-      if(order.get('dispatchedOrdersPackages').length) {
-        this.genericAlertPopUp("order_details.dispatch_later_undispatch_warning", function() { _this.send("toggleDisplayOptions"); });
+      if (order.get("dispatchedOrdersPackages").length) {
+        this.genericAlertPopUp(
+          "order_details.dispatch_later_undispatch_warning",
+          function() {
+            _this.send("toggleDisplayOptions");
+          }
+        );
       } else {
-        this.genericCustomPopUp("order_details.dispatch_later_warning", "order.dispatch_later", "not_now", function() { _this.send("changeOrderState", order, actionName); });
+        this.genericCustomPopUp(
+          "order_details.dispatch_later_warning",
+          "order.dispatch_later",
+          "not_now",
+          function() {
+            _this.send("changeOrderState", order, actionName);
+          }
+        );
       }
     },
 
     promptResubmitModel(order, actionName) {
       var _this = this;
-      this.genericCustomPopUp("order_details.resubmit_order_warning", "order.resubmit", "not_now", function() { _this.send("changeOrderState", order, actionName); });
+      this.genericCustomPopUp(
+        "order_details.resubmit_order_warning",
+        "order.resubmit",
+        "not_now",
+        function() {
+          _this.send("changeOrderState", order, actionName);
+        }
+      );
     },
 
     promptReopenModel(order, actionName) {
       var _this = this;
-      this.genericCustomPopUp("order_details.reopen_warning", "order.reopen_order", "not_now", function() { _this.send("changeOrderState", order, actionName); });
+      this.genericCustomPopUp(
+        "order_details.reopen_warning",
+        "order.reopen_order",
+        "not_now",
+        function() {
+          _this.send("changeOrderState", order, actionName);
+        }
+      );
     },
 
     toggleDisplayOptions() {
-      if(this.get("displayOrderOptions")) {
+      if (this.get("displayOrderOptions")) {
         this.set("displayOrderOptions", false);
       }
     },
 
     promptRestartProcessModel(order, actionName) {
       var _this = this;
-      if(order.get('dispatchedOrdersPackages').length) {
-        this.genericAlertPopUp("order_details.restart_undispatch_warning", function() { _this.send("toggleDisplayOptions"); });
+      if (order.get("dispatchedOrdersPackages").length) {
+        this.genericAlertPopUp(
+          "order_details.restart_undispatch_warning",
+          function() {
+            _this.send("toggleDisplayOptions");
+          }
+        );
       } else {
-        this.genericCustomPopUp("order_details.restart_warning", "order.restart_process", "not_now", function() { _this.set("isOrderProcessRestarted", true); _this.send("changeOrderState", order, actionName); });
+        this.genericCustomPopUp(
+          "order_details.restart_warning",
+          "order.restart_process",
+          "not_now",
+          function() {
+            _this.set("isOrderProcessRestarted", true);
+            _this.send("changeOrderState", order, actionName);
+          }
+        );
       }
     },
 
     promptCancelOrderModel(order, actionName) {
       var _this = this;
-      if(this.canCancelOrder(order)) {
-        this.genericCustomPopUp("order_details.cancel_warning", "order.cancel_order", "not_now", function() { _this.send("changeOrderState", order, actionName); });
+      if (this.canCancelOrder(order)) {
+        this.genericCustomPopUp(
+          "order_details.cancel_warning",
+          "order.cancel_order",
+          "not_now",
+          function() {
+            _this.send("changeOrderState", order, actionName);
+          }
+        );
       } else {
-        this.genericAlertPopUp("order_details.cancel_order_alert", function() {});
+        this.genericAlertPopUp(
+          "order_details.cancel_order_alert",
+          function() {}
+        );
       }
     },
 
     promptCloseOrderModel(order, actionName) {
       var _this = this;
-      if(this.canCloseOrder(order)) {
-        this.genericCustomPopUp("order_details.close_warning", "order.close_order", "not_now", function() { _this.send("changeOrderState", order, actionName); });
+      if (this.canCloseOrder(order)) {
+        this.genericCustomPopUp(
+          "order_details.close_warning",
+          "order.close_order",
+          "not_now",
+          function() {
+            _this.send("changeOrderState", order, actionName);
+          }
+        );
       } else {
-        this.genericAlertPopUp("order_details.close_order_dispatch_alert", function() {});
+        this.genericAlertPopUp(
+          "order_details.close_order_dispatch_alert",
+          function() {}
+        );
       }
     },
 
     changeOrderState(order, transition) {
       var url = `/orders/${order.id}/transition`;
-      var loadingView = getOwner(this).lookup('component:loading').append();
-      new AjaxPromise(url, "PUT", this.get('session.authToken'), { transition: transition })
+      var loadingView = getOwner(this)
+        .lookup("component:loading")
+        .append();
+      new AjaxPromise(url, "PUT", this.get("session.authToken"), {
+        transition: transition
+      })
         .then(data => {
-          if("transition" === "restart_process") {
+          if ("transition" === "restart_process") {
             this.set("isOrderProcessRestarted", false);
           }
           this.send("toggleDisplayOptions");
@@ -260,11 +376,10 @@ export default GoodcityController.extend({
         })
         .finally(() => {
           loadingView.destroy();
-          if(transition === "close") {
+          if (transition === "close") {
             this.get("appReview").promptReviewModal(true);
           }
         });
     }
   }
-
 });
