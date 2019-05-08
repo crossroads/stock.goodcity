9ad92c2483c366bd8ec8803e0d3f2dd2ef9180eb
namrataukirde <namrata.ukirde@kiprosh.com>
2019-05-08 20:06:02 +0530

    Revert "fixed invalid association name"
    This reverts commit e95c84da3984138f769ce424b14b48c73484bda5.

--- app/models/orders_process_checklist.js
+++ app/models/orders_process_checklist.js
@@ -3,13 +3,8 @@ import attr from "ember-data/attr";
 import { belongsTo } from "ember-data/relationships";
 
 export default Model.extend({
-  order: belongsTo("designation", {
-    async: false
-  }),
+  order: belongsTo("booking_type", { async: false }),
   orderId: attr("number"),
-  designationId: attr("number"),
-  processChecklist: belongsTo("booking_type", {
-    async: false
-  }),
+  processChecklist: belongsTo("booking_type", { async: false }),
   processChecklistId: attr("number")
 });
