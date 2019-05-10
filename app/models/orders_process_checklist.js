61df0116682715b5264a8bb7dc0d289fa3876cf7
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2019-05-10 09:37:37 +0530

    association name fix

--- app/models/orders_process_checklist.js
+++ app/models/orders_process_checklist.js
@@ -3,7 +3,7 @@ import attr from "ember-data/attr";
 import { belongsTo } from "ember-data/relationships";
 
 export default Model.extend({
-  order: belongsTo("booking_type", { async: false }),
+  order: belongsTo("designation", { async: false }),
   orderId: attr("number"),
   processChecklist: belongsTo("booking_type", { async: false }),
   processChecklistId: attr("number")
