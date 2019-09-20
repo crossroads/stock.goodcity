d24152842f800dca419c7451741c803d70281881
AbulAsar <abul.a@kiprosh.com>
2019-09-20 17:45:42 +0530

    removed set property on itemSetId

--- tests/controller/items/index-test.js
+++ tests/controller/items/index-test.js
@@ -1,28 +1,27 @@
-import { test, moduleFor } from 'ember-qunit';
-import startApp from '../../helpers/start-app';
-import TestHelper from 'ember-data-factory-guy/factory-guy-test-helper';
-import Ember from 'ember';
+import { test, moduleFor } from "ember-qunit";
+import startApp from "../../helpers/start-app";
+import TestHelper from "ember-data-factory-guy/factory-guy-test-helper";
+import Ember from "ember";
 
 var App;
 
-moduleFor('controller:items.index', 'items.index controller', {
+moduleFor("controller:items.index", "items.index controller", {
   beforeEach: function() {
     App = startApp({}, 2);
     TestHelper.setup();
   },
   afterEach: function() {
-    Ember.run(function() { TestHelper.teardown(); });
-    Ember.run(App, 'destroy');
+    Ember.run(function() {
+      TestHelper.teardown();
+    });
+    Ember.run(App, "destroy");
   }
 });
 
-test('Checking for default set values', function(assert) {
+test("Checking for default set values", function(assert) {
   assert.expect(1);
 
   var ctrl = this.subject();
 
-  ctrl.set('itemSetId', "1");
-
-  assert.equal(ctrl.get('searchModelName'), "item");
+  assert.equal(ctrl.get("searchModelName"), "item");
 });
-
