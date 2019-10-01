061a37636c4c254db2c225e4354d90f6ab3dd85a
akshay gaikwad <akshay@kiprosh.com>
2019-10-01 14:46:52 +0530

    GCW-2804 created a computed property to get all options for the dropdown

--- app/components/dropdown-search.js
+++ app/components/dropdown-search.js
@@ -16,49 +16,12 @@ export default Ember.Component.extend({
   selectedTags: [],
   numTags: Ember.computed.alias("talkTags.length"),
   selectedOptionData: " ",
+
   resourceType: Ember.computed("packageDetails", function() {
-    debugger;
     console.log(this.get("packageDetails"), "hit123");
     return this.get("packageDetails");
-    // return {
-    //   size: [{
-    //       id: 0,
-    //       tag: "1x"
-    //     },
-    //     {
-    //       id: 0,
-    //       tag: "2x"
-    //     }
-    //   ],
-    //   voltage: [{
-    //       id: 0,
-    //       tag: "1V"
-    //     },
-    //     {
-    //       id: 1,
-    //       tag: "2V"
-    //     }
-    //   ],
-    //   power: [{
-    //       id: 0,
-    //       tag: "100power"
-    //     },
-    //     {
-    //       id: 2,
-    //       tag: "200power"
-    //     }
-    //   ],
-    //   brand: [{
-    //       id: 0,
-    //       tag: "Lenovo"
-    //     },
-    //     {
-    //       id: 2,
-    //       tag: "Philipes"
-    //     }
-    //   ]
-    // };
   }),
+
   actions: {
     addNew(fieldName, text) {
       this.set(fieldName, text);
