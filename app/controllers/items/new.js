56974e6330ec884bc2a5a2b32eb0c1889beb89c7
akshay gaikwad <akshay@kiprosh.com>
2019-10-11 17:06:08 +0530

    GCW-2804 used service to store data and on save empty fields object

--- app/controllers/items/new.js
+++ app/controllers/items/new.js
@@ -19,6 +19,7 @@ export default GoodcityController.extend({
   locationName: Ember.computed.alias("location.displayName"),
   caseNumber: "",
   isSearchCodePreviousRoute: Ember.computed.localStorage(),
+  fields: Ember.inject.service("additional-fields"),
   weight: "",
   isSelectLocationPreviousRoute: Ember.computed.localStorage(),
   quantity: 1,
@@ -44,276 +45,7 @@ export default GoodcityController.extend({
 
   imageKeys: Ember.computed.localStorage(),
 
-  // detailParams: function(){
-  //   this.get("additionalFields").forEach(field)
-  //   {this.get(field.name)};
-  // },
-
   i18n: Ember.inject.service(),
-
-  additionalFields: [
-    {
-      label: "Brand",
-      name: "brand",
-      value: "brand",
-      type: "text",
-      autoComplete: true,
-      category: ["computer", "computer_accessory", "electrical"],
-      addAble: true
-    },
-    {
-      label: "Model",
-      name: "model",
-      type: "text",
-      value: "model",
-      autoComplete: false,
-      category: ["computer", "computer_accessory", "electrical"],
-      addAble: false
-    },
-    {
-      label: "Serial Number",
-      name: "serialNumber",
-      type: "text",
-      value: "serialNumber",
-      autoComplete: false,
-      category: ["computer", "computer_accessory", "electrical"],
-      addAble: false
-    },
-    {
-      label: "Country of origin",
-      name: "country",
-      type: "number",
-      value: "country",
-      autoComplete: true,
-      category: ["computer", "computer_accessory", "electrical"],
-      addAble: false
-    },
-    {
-      label: "Size",
-      name: "size",
-      type: "text",
-      value: "size",
-      autoComplete: true,
-      category: ["computer", "computer_accessory"],
-      addAble: true
-    },
-    {
-      label: "Cpu",
-      name: "cpu",
-      type: "text",
-      value: "cpu",
-      autoComplete: true,
-      category: ["computer"],
-      addAble: true
-    },
-    {
-      label: "Comp Test Status",
-      name: "compTestStatus",
-      type: "text",
-      value: "compTestStatus",
-      autoComplete: true,
-      category: ["computer", "computer_accessory"],
-      addAble: false
-    },
-    {
-      label: "Ram",
-      name: "ram",
-      type: "text",
-      value: "ram",
-      autoComplete: true,
-      category: ["computer"],
-      addAble: true
-    },
-    {
-      label: "HDD",
-      name: "hdd",
-      type: "text",
-      value: "hdd",
-      autoComplete: true,
-      category: ["computer"],
-      addAble: true
-    },
-    {
-      label: "Optical",
-      name: "optical",
-      type: "text",
-      value: "optical",
-      autoComplete: true,
-      category: ["computer"],
-      addAble: true
-    },
-    {
-      label: "video",
-      name: "video",
-      type: "text",
-      value: "video",
-      autoComplete: true,
-      category: ["computer"],
-      addAble: true
-    },
-    {
-      label: "Sound",
-      name: "sound",
-      type: "text",
-      value: "sound",
-      autoComplete: true,
-      category: ["computer"],
-      addAble: true
-    },
-    {
-      label: "Lan",
-      name: "lan",
-      type: "text",
-      value: "lan",
-      autoComplete: true,
-      category: ["computer"],
-      addAble: true
-    },
-    {
-      label: "Wireless",
-      name: "wireless",
-      type: "text",
-      value: "wireless",
-      autoComplete: true,
-      category: ["computer"],
-      addAble: true
-    },
-    {
-      label: "Usb",
-      name: "usb",
-      type: "text",
-      value: "usb",
-      autoComplete: true,
-      category: ["computer"],
-      addAble: true
-    },
-    {
-      label: "Comp Voltage",
-      name: "compVoltage",
-      type: "text",
-      value: "compVoltage",
-      autoComplete: true,
-      category: ["computer", "computer_accessory"],
-      addAble: true
-    },
-    {
-      label: "OS",
-      name: "os",
-      type: "text",
-      value: "os",
-      autoComplete: true,
-      category: ["computer"],
-      addAble: true
-    },
-    {
-      label: "OS Serial Num",
-      name: "os_serial_num",
-      type: "text",
-      value: "osSerialNum",
-      autoComplete: false,
-      category: ["computer"],
-      addAble: false
-    },
-    {
-      label: "Ms Office Serial Num",
-      name: "ms_office_serial_num",
-      type: "text",
-      value: "msOfficeSerialNum",
-      autoComplete: false,
-      category: ["computer"],
-      addAble: false
-    },
-    {
-      label: "Mar  OS serial Num",
-      name: "mar_os_serial_num",
-      type: "text",
-      value: "marOsSerialNum",
-      autoComplete: false,
-      category: ["computer"],
-      addAble: false
-    },
-    {
-      label: "Mar Ms Office Serial Num",
-      name: "mar_ms_office_serial_num",
-      type: "text",
-      value: "marMsOfficeSerialNum",
-      autoComplete: false,
-      category: ["computer"],
-      addAble: false
-    },
-    {
-      label: "Interface",
-      name: "interface",
-      type: "text",
-      value: "interface",
-      autoComplete: true,
-      category: ["computer_accessory"],
-      addAble: false
-    },
-    {
-      label: "Standard",
-      name: "standard",
-      type: "text",
-      value: "standard",
-      autoComplete: true,
-      category: ["computer_accessory"],
-      addAble: true
-    },
-    {
-      label: "Voltage",
-      name: "voltage",
-      type: "number",
-      value: "voltage",
-      autoComplete: true,
-      category: ["electrical"],
-      addAble: false
-    },
-    {
-      label: "Power",
-      name: "power",
-      type: "text",
-      value: "power",
-      autoComplete: true,
-      category: ["electrical"],
-      addAble: true
-    },
-    {
-      label: "System or Region",
-      name: "systemOrRegion",
-      type: "text",
-      value: "systemOrRegion",
-      autoComplete: true,
-      category: ["electrical"],
-      addAble: true
-    },
-    {
-      label: "Standard",
-      name: "standard",
-      type: "text",
-      value: "Standard",
-      autoComplete: false,
-      category: ["electrical"],
-      addAble: true
-    },
-    {
-      label: "Test Status",
-      name: "testStatus",
-      type: "text",
-      value: "testStatus",
-      autoComplete: true,
-      category: ["electrical"],
-      addAble: false
-    },
-    {
-      label: "Frequency",
-      name: "frequency",
-      type: "number",
-      value: "frequency",
-      autoComplete: true,
-      category: ["electrical"],
-      addAble: false
-    }
-  ],
 
   showAdditionalFields: Ember.computed("code", function() {
     return !(
@@ -385,7 +117,7 @@ export default GoodcityController.extend({
   displayFields: Ember.computed("code", function() {
     let _this = this;
     if (this.get("showAdditionalFields")) {
-      return this.additionalFields.filter(function(field) {
+      return this.get("fields").additionalFields.filter(function(field) {
         return field.category.includes(_this.get("code.subform"));
       });
     }
@@ -952,6 +684,9 @@ export default GoodcityController.extend({
         ...this.get("fieldValues"),
         ...this.get("selectedCountry")
       };
+      this.set("formElement", {});
+      this.set("fieldsValues", {});
+      this.set("selectedCountry", {});
 
       polymorphicField["detail_type"] = this.get("code.subform");
       this.set("subFormDataObj", subFormDataObj);
---
+++ app/services/additional-fields.js
@@ -0,0 +1,268 @@
+import Ember from "ember";
+import AjaxPromise from "./../utils/ajax-promise";
+
+export default Ember.Service.extend({
+  additionalFields: [
+    {
+      label: "Brand",
+      name: "brand",
+      value: "brand",
+      type: "text",
+      autoComplete: true,
+      category: ["computer", "computer_accessory", "electrical"],
+      addAble: true
+    },
+    {
+      label: "Model",
+      name: "model",
+      type: "text",
+      value: "model",
+      autoComplete: false,
+      category: ["computer", "computer_accessory", "electrical"],
+      addAble: false
+    },
+    {
+      label: "Serial Number",
+      name: "serialNumber",
+      type: "text",
+      value: "serialNumber",
+      autoComplete: false,
+      category: ["computer", "computer_accessory", "electrical"],
+      addAble: false
+    },
+    {
+      label: "Country of origin",
+      name: "country",
+      type: "number",
+      value: "country",
+      autoComplete: true,
+      category: ["computer", "computer_accessory", "electrical"],
+      addAble: false
+    },
+    {
+      label: "Size",
+      name: "size",
+      type: "text",
+      value: "size",
+      autoComplete: true,
+      category: ["computer", "computer_accessory"],
+      addAble: true
+    },
+    {
+      label: "Cpu",
+      name: "cpu",
+      type: "text",
+      value: "cpu",
+      autoComplete: true,
+      category: ["computer"],
+      addAble: true
+    },
+    {
+      label: "Comp Test Status",
+      name: "compTestStatus",
+      type: "text",
+      value: "compTestStatus",
+      autoComplete: true,
+      category: ["computer", "computer_accessory"],
+      addAble: false
+    },
+    {
+      label: "Ram",
+      name: "ram",
+      type: "text",
+      value: "ram",
+      autoComplete: true,
+      category: ["computer"],
+      addAble: true
+    },
+    {
+      label: "HDD",
+      name: "hdd",
+      type: "text",
+      value: "hdd",
+      autoComplete: true,
+      category: ["computer"],
+      addAble: true
+    },
+    {
+      label: "Optical",
+      name: "optical",
+      type: "text",
+      value: "optical",
+      autoComplete: true,
+      category: ["computer"],
+      addAble: true
+    },
+    {
+      label: "video",
+      name: "video",
+      type: "text",
+      value: "video",
+      autoComplete: true,
+      category: ["computer"],
+      addAble: true
+    },
+    {
+      label: "Sound",
+      name: "sound",
+      type: "text",
+      value: "sound",
+      autoComplete: true,
+      category: ["computer"],
+      addAble: true
+    },
+    {
+      label: "Lan",
+      name: "lan",
+      type: "text",
+      value: "lan",
+      autoComplete: true,
+      category: ["computer"],
+      addAble: true
+    },
+    {
+      label: "Wireless",
+      name: "wireless",
+      type: "text",
+      value: "wireless",
+      autoComplete: true,
+      category: ["computer"],
+      addAble: true
+    },
+    {
+      label: "Usb",
+      name: "usb",
+      type: "text",
+      value: "usb",
+      autoComplete: true,
+      category: ["computer"],
+      addAble: true
+    },
+    {
+      label: "Comp Voltage",
+      name: "compVoltage",
+      type: "text",
+      value: "compVoltage",
+      autoComplete: true,
+      category: ["computer", "computer_accessory"],
+      addAble: true
+    },
+    {
+      label: "OS",
+      name: "os",
+      type: "text",
+      value: "os",
+      autoComplete: true,
+      category: ["computer"],
+      addAble: true
+    },
+    {
+      label: "OS Serial Num",
+      name: "os_serial_num",
+      type: "text",
+      value: "osSerialNum",
+      autoComplete: false,
+      category: ["computer"],
+      addAble: false
+    },
+    {
+      label: "Ms Office Serial Num",
+      name: "ms_office_serial_num",
+      type: "text",
+      value: "msOfficeSerialNum",
+      autoComplete: false,
+      category: ["computer"],
+      addAble: false
+    },
+    {
+      label: "Mar  OS serial Num",
+      name: "mar_os_serial_num",
+      type: "text",
+      value: "marOsSerialNum",
+      autoComplete: false,
+      category: ["computer"],
+      addAble: false
+    },
+    {
+      label: "Mar Ms Office Serial Num",
+      name: "mar_ms_office_serial_num",
+      type: "text",
+      value: "marMsOfficeSerialNum",
+      autoComplete: false,
+      category: ["computer"],
+      addAble: false
+    },
+    {
+      label: "Interface",
+      name: "interface",
+      type: "text",
+      value: "interface",
+      autoComplete: true,
+      category: ["computer_accessory"],
+      addAble: false
+    },
+    {
+      label: "Standard",
+      name: "standard",
+      type: "text",
+      value: "standard",
+      autoComplete: true,
+      category: ["computer_accessory"],
+      addAble: true
+    },
+    {
+      label: "Voltage",
+      name: "voltage",
+      type: "number",
+      value: "voltage",
+      autoComplete: true,
+      category: ["electrical"],
+      addAble: false
+    },
+    {
+      label: "Power",
+      name: "power",
+      type: "text",
+      value: "power",
+      autoComplete: true,
+      category: ["electrical"],
+      addAble: true
+    },
+    {
+      label: "System or Region",
+      name: "systemOrRegion",
+      type: "text",
+      value: "systemOrRegion",
+      autoComplete: true,
+      category: ["electrical"],
+      addAble: true
+    },
+    {
+      label: "Standard",
+      name: "standard",
+      type: "text",
+      value: "Standard",
+      autoComplete: false,
+      category: ["electrical"],
+      addAble: true
+    },
+    {
+      label: "Test Status",
+      name: "testStatus",
+      type: "text",
+      value: "testStatus",
+      autoComplete: true,
+      category: ["electrical"],
+      addAble: false
+    },
+    {
+      label: "Frequency",
+      name: "frequency",
+      type: "number",
+      value: "frequency",
+      autoComplete: true,
+      category: ["electrical"],
+      addAble: false
+    }
+  ]
+});
