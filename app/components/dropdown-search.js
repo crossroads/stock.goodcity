8add11c0a10b2a65e7fbfc4bfdc6b60ebf903fe9
AbulAsar <abul.a@kiprosh.com>
2019-10-15 21:17:19 +0530

    select drop down component refactoring done

--- app/components/dropdown-search.js
+++ app/components/dropdown-search.js
@@ -2,51 +2,26 @@
 import Ember from "ember";
 
 export default Ember.Component.extend({
-  // classNames: "Example",
   selected: [],
-  selectedTags: [],
-  optionObject: {},
-  selectedOptionData: " ",
-  disbale: true,
 
-  resourceType: Ember.computed("packageDetails", function() {
-    return this.get("packageDetails");
-  }),
-
-  selectedData: Ember.computed("selectedValues", function() {
-    return this.get("selectedValues");
-  }),
-
-  selectedDataDisplay: Ember.computed("selectedValuesDisplay", function() {
-    return this.get("selectedValuesDisplay");
-  }),
-
-  displayPage: Ember.computed("displayPage", function() {
-    return this.get("displayPage");
-  }),
+  resourceType: Ember.computed.alias("packageDetails"),
+  selectedData: Ember.computed.alias("selectedValue"),
 
   actions: {
     addNew(fieldName, text) {
-      this.set(fieldName, text);
-      let data = this.get("packageDetails");
-      let newTag = {
-        id: data[fieldName].length + 1,
+      let packageDetails = this.get("packageDetails");
+      const newTag = {
+        id: packageDetails[fieldName].length + 1,
         tag: text
       };
-      data[fieldName].push({
-        id: data[fieldName].length + 1,
-        tag: text
-      });
       this.set("selected", newTag);
-      this.set("packageDetails", data);
+      this.set("packageDetails", packageDetails[fieldName].push(newTag));
       this.send("setSelected", fieldName, newTag);
     },
 
     setSelected(fieldName, value) {
-      let optionObj = this.get("optionObject");
-      optionObj[fieldName] = value.tag;
-      this.set("optionObject", optionObj);
-      this.get("onConfirm")(this.get("optionObject"));
+      const newOption = { [fieldName]: value.tag };
+      this.get("onConfirm")(newOption);
     }
   }
 });
