256d7afbb5cb9fc111dc16b23545cedc08078790
akshay gaikwad <akshay@kiprosh.com>
2019-10-16 20:01:50 +0530

    GCW-2804 inline input done for input for input fields

--- app/components/additinal-fields-input.js
+++ app/components/additinal-fields-input.js
@@ -2,8 +2,13 @@ import Ember from "ember";
 import AjaxPromise from "stock/utils/ajax-promise";
 import config from "../config/environment";
 import Inflector from "ember-inflector";
-import { singularize, pluralize } from "ember-inflector";
-const { getOwner } = Ember;
+import {
+  singularize,
+  pluralize
+} from "ember-inflector";
+const {
+  getOwner
+} = Ember;
 
 export default Ember.TextField.extend({
   tagName: "input",
@@ -21,7 +26,50 @@ export default Ember.TextField.extend({
     "pattern"
   ],
   store: Ember.inject.service(),
+  item: null,
+  classNameBindings: ["class"],
   previousValue: "",
-  displayScanner: false,
-  item: null
+
+  focusOut() {
+    var item = this.get("item");
+    let detailType = pluralize(this.get("detailType")).toLowerCase();
+    let detailId = this.get("detailId");
+    var url = `/${detailType}/${detailId}`;
+    console.log(url);
+    var key = this.get("name");
+    console.log(key, "hit");
+    var packageParams = {};
+    packageParams[key] = this.get("value") || "";
+    console.log(this.get("previousValue"), "hit");
+    Ember.$(this.element).removeClass("inline-text-input");
+    if (
+      packageParams[key].toString() !== this.get("previousValue").toString()
+    ) {
+      var loadingView = getOwner(this)
+        .lookup("component:loading")
+        .append();
+      new AjaxPromise(url, "PUT", this.get("session.authToken"), {
+          package: packageParams
+        })
+        .then(data => {
+          this.get("store").pushPayload(data);
+        })
+        .finally(() => {
+          loadingView.destroy();
+        });
+    }
+  },
+
+  focusIn() {
+    this.addCssStyle();
+  },
+
+  addCssStyle() {
+    Ember.$(this.element).addClass("inline-text-input");
+    this.set("previousValue", this.get("value") || "");
+  },
+
+  click() {
+    this.addCssStyle();
+  }
 });
