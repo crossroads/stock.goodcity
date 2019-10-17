5df948955990cb5feea44787a4c207d3a74a2fc2
Abhinav Garg <abhinav.g@kiprosh.com>
2019-10-17 13:06:31 +0530

    fix endpoint and params for update

--- app/components/additinal-fields-input.js
+++ app/components/additinal-fields-input.js
@@ -4,6 +4,7 @@ import config from "../config/environment";
 import Inflector from "ember-inflector";
 import { singularize, pluralize } from "ember-inflector";
 const { getOwner } = Ember;
+import _ from "lodash";
 
 export default Ember.TextField.extend({
   tagName: "input",
@@ -21,30 +22,27 @@ export default Ember.TextField.extend({
     "pattern"
   ],
   store: Ember.inject.service(),
-  item: null,
   classNameBindings: ["class"],
   previousValue: "",
 
   focusOut() {
-    var item = this.get("item");
-    let detailType = pluralize(this.get("detailType")).toLowerCase();
+    let detailType = this.get("detailType").toLowerCase();
+    let apiEndpoint = pluralize(detailType);
     let detailId = this.get("detailId");
-    var url = `/${detailType}/${detailId}`;
-    console.log(url);
+    var url = `/${apiEndpoint}/${detailId}`;
     var key = this.get("name");
-    console.log(key, "hit");
-    var packageParams = {};
-    packageParams[key] = this.get("value") || "";
-    console.log(this.get("previousValue"), "hit");
+    var packageDetailParams = {
+      [_.snakeCase(key)]: this.get("value") || ""
+    };
     Ember.$(this.element).removeClass("inline-text-input");
     if (
-      packageParams[key].toString() !== this.get("previousValue").toString()
+      this.valueChanged(packageDetailParams[key], this.get("previousValue"))
     ) {
       var loadingView = getOwner(this)
         .lookup("component:loading")
         .append();
       new AjaxPromise(url, "PUT", this.get("session.authToken"), {
-        package: packageParams
+        [detailType]: packageDetailParams
       })
         .then(data => {
           this.get("store").pushPayload(data);
@@ -53,6 +51,10 @@ export default Ember.TextField.extend({
           loadingView.destroy();
         });
     }
+  },
+
+  valueChanged(newValue, previousValue) {
+    return newValue !== previousValue;
   },
 
   focusIn() {
