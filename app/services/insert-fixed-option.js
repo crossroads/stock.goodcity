011b9d9edf504ab3c7817e89266167da4c5a8ecf
akshay gaikwad <akshay@kiprosh.com>
2019-10-31 15:57:55 +0530

    GCW-2804 used preloading to fetch fixed options of dropdown

--- app/services/insert-fixed-option.js
+++ app/services/insert-fixed-option.js
@@ -1,36 +1,24 @@
 import Ember from "ember";
-import DS from "ember-data";
-import AjaxPromise from "./../utils/ajax-promise";
 import _ from "lodash";
-const { getOwner } = Ember;
 
 export default Ember.Service.extend({
   session: Ember.inject.service(),
   store: Ember.inject.service(),
 
   fixedOptionDropDown(column, package_details) {
-    switch (column) {
-      case "frequency":
-        return ["50", "50/60 (Multi)", "60", "N/A", "Other"];
-      case "voltage":
-        return [
-          "120V ~ (100-200V)",
-          "240V ~ (200-300V)",
-          "> 300V",
-          "Multi",
-          "NA",
-          "Other"
-        ];
-      case "testStatus":
-        return [
-          "Maintenance (DO NOT USE)",
-          "Tested (DO NOT USE)",
-          "Untested (DO NOT USE)"
-        ];
-      case "compTestStatus":
-        return ["Active", "Failure", "Obsolete", "Reserved", "Spares"];
-      default:
-        return [...new Set(package_details.getEach(column).filter(Boolean))];
+    if (["frequency", "voltage", "testStatus"].indexOf(column) > -1) {
+      return this.fetchLookups(`electrical_${_.snakeCase(column)}`);
+    } else if (column === "compTestStatus") {
+      return this.fetchLookups("comp_test_status");
+    } else {
+      return [...new Set(package_details.getEach(column).filter(Boolean))];
     }
+  },
+
+  fetchLookups(lookupName) {
+    return this.get("store")
+      .peekAll("lookup")
+      .filterBy("name", lookupName)
+      .getEach("labelEn");
   }
 });
