060a7eab4779681270d15cb58c0ac717b4aa9484
AbulAsar Sayyad <abul.a@kiprosh.com>
2019-11-22 20:51:04 +0530

    delete detail type logic added

--- app/controllers/items/search_code.js
+++ app/controllers/items/search_code.js
@@ -1,6 +1,8 @@
 import Ember from "ember";
 import SearchCode from "../search_code";
 import AjaxPromise from "stock/utils/ajax-promise";
+import { pluralize } from "ember-inflector";
+import _ from "lodash";
 const { getOwner } = Ember;
 
 export default SearchCode.extend({
@@ -17,29 +19,47 @@ export default SearchCode.extend({
     }
   }),
 
+  getDetailURL() {
+    const detailType = _.snakeCase(this.get("item.detailType")).toLowerCase();
+    const apiEndpoint = pluralize(detailType);
+    const detailId = this.get("item.detail.id");
+    return `/${apiEndpoint}/${detailId}`;
+  },
+
   packageTypeSelectWarning(newPkgType) {
     const item = this.get("item");
     const pkgType = item.get("code");
     const packageName = pkgType.get("name");
     const newPackage = newPkgType.get("name");
-    const url = `/packages/${item.get("id")}/remove_details`;
+
     this.get("messageBox").custom(
       `If you change to ${newPackage} some details related to ${packageName} will no longer be valid. These details will be deleted.`,
       "Not Now",
       null,
       "Continue",
       () => {
-        console.log("Continue selected");
-        if (this.isSubformPackage(pkgType)) {
+        if (
+          this.isSubformPackage(pkgType) &&
+          this.isSubformPackage(newPkgType)
+        ) {
           //first delete subform
-          new AjaxPromise(url, "PUT", this.get("session.authToken")).then(
-            () => {
-              // PUT request of new packageType
-              this.assignNewType(newPkgType);
-            }
-          );
+          new AjaxPromise(
+            this.getDetailURL(),
+            "DELETE",
+            this.get("session.authToken")
+          ).then(() => {
+            // PUT request of new packageType
+            this.assignNewType(newPkgType);
+          });
         } else {
-          //
+          new AjaxPromise(
+            this.getDetailURL(),
+            "DELETE",
+            this.get("session.authToken")
+          ).then(() => {
+            // PUT request of new packageType
+            this.assignNewType(newPkgType);
+          });
         }
       }
     );
@@ -50,13 +70,15 @@ export default SearchCode.extend({
   },
 
   assignNewType(type) {
-    var item = this.get("item");
-    var url = `/packages/${item.get("id")}`;
-    var key = "package_type_id";
-    var packageParams = {};
-    packageParams[key] = type.get("id");
+    const item = this.get("item");
+    const url = `/packages/${item.get("id")}`;
+    const packageParams = {
+      package_type_id: type.get("id"),
+      detail_id: null,
+      detail_type: null
+    };
 
-    var loadingView = getOwner(this)
+    const loadingView = getOwner(this)
       .lookup("component:loading")
       .append();
     new AjaxPromise(url, "PUT", this.get("session.authToken"), {
