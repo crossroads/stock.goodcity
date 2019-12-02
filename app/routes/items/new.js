1db79311c404653159f401d7a8ea4e0f0a3c30e2
AbulAsar Sayyad <abul.a@kiprosh.com>
2019-12-02 18:27:31 +0530

    on refresh subform getting dissappeared issue fixed | clean setup_controller

--- app/routes/items/new.js
+++ app/routes/items/new.js
@@ -54,70 +54,101 @@ export default AuthorizeRoute.extend({
     });
   },
 
-  async setupController(controller, model) {
+  setupController(controller, model) {
     this._super(controller, model);
     const store = this.get("store");
+    this.initializeController();
+
+    if (this.get("newItemRequest")) {
+      this.initializeDimensions();
+      this.manageSubformDetails();
+      this.setUpPackageImage();
+      window.localStorage.setItem("isSelectLocationPreviousRoute", false);
+    }
+  },
+
+  initializeController() {
+    const controller = this.controller;
     if (!controller.get("inventoryNumber")) {
       controller.send("autoGenerateInventoryNumber");
     }
     controller.set("invalidLocation", false);
     controller.set("invalidScanResult", false);
     controller.set("labels", 1);
+  },
 
-    if (this.get("newItemRequest")) {
-      this.set("newItemRequest", false);
-      controller.set("quantity", 1);
-      if (
-        window.localStorage.getItem("isSelectLocationPreviousRoute") === "false"
-      ) {
-        controller.set("caseNumber", "");
-        controller.set("length", null);
-        controller.set("width", null);
-        controller.set("height", null);
-        controller.set("selectedGrade", {
-          name: "B",
-          id: "B"
-        });
-        controller.set("imageKeys", "");
-      }
-      let codeId = controller.get("codeId");
-      if (codeId) {
-        let selected = store.peekRecord("code", codeId);
-        if (selected) {
-          let selectedSubform = selected.get("subform");
-          if (this.isSubformAllowed(selectedSubform)) {
-            controller.set("showAdditionalFields", true);
-            let details = await store.query(selectedSubform, {
-              distinct: "brand"
+  async manageSubformDetails() {
+    const controller = this.controller;
+    const store = this.store;
+    let codeId = controller.get("codeId");
+    if (codeId) {
+      let selected = store.peekRecord("code", codeId);
+      if (selected) {
+        let selectedSubform = selected.get("subform");
+        if (this.isSubformAllowed(selectedSubform)) {
+          controller.set("showAdditionalFields", true);
+          let details = await store.query(selectedSubform, {
+            distinct: "brand"
+          });
+          if (this.get("transitionFrom") === "code") {
+            controller.setProperties({
+              dropDownValues: {},
+              countryValue: {},
+              selected: []
             });
             controller.set("packageDetails", details);
           } else {
             controller.set("showAdditionalFields", false);
           }
+          controller.set("packageDetails", details);
+        } else {
+          controller.set("showAdditionalFields", false);
         }
       }
+    }
+  },
 
-      var imageKey = controller.get("imageKeys");
-      if (
-        imageKey &&
-        imageKey.length &&
-        window.localStorage.isSelectLocationPreviousRoute === "true"
-      ) {
-        var image = store
-          .peekAll("image")
-          .filterBy("cloudinaryId", imageKey)
-          .get("firstObject");
-        image =
-          image ||
-          store.createRecord("image", {
-            cloudinaryId: imageKey,
-            favourite: true
-          });
-        controller.set("newUploadedImage", image);
-      } else {
-        controller.set("newUploadedImage", null);
-      }
-      window.localStorage.setItem("isSelectLocationPreviousRoute", false);
+  setUpPackageImage() {
+    const controller = this.controller;
+    const imageKey = controller.get("imageKeys");
+    const store = this.store;
+    if (
+      imageKey &&
+      imageKey.length &&
+      window.localStorage.isSelectLocationPreviousRoute === "true"
+    ) {
+      let image = store
+        .peekAll("image")
+        .filterBy("cloudinaryId", imageKey)
+        .get("firstObject");
+      image =
+        image ||
+        store.createRecord("image", {
+          cloudinaryId: imageKey,
+          favourite: true
+        });
+      controller.set("newUploadedImage", image);
+    } else {
+      controller.set("newUploadedImage", null);
+    }
+  },
+
+  initializeDimensions() {
+    const controller = this.controller;
+    this.set("newItemRequest", false);
+    controller.set("quantity", 1);
+    if (
+      window.localStorage.getItem("isSelectLocationPreviousRoute") === "false"
+    ) {
+      controller.set("caseNumber", "");
+      controller.set("length", null);
+      controller.set("width", null);
+      controller.set("height", null);
+      controller.set("selectedGrade", {
+        name: "B",
+        id: "B"
+      });
+      controller.set("imageKeys", "");
     }
   }
 });
