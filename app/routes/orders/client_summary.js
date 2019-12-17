c270361393ebf56c287bf599e5d97f5c4b637a99
Patrick R <patrixr@Patricks-MacBook-Pro.local>
2019-12-17 16:04:03 +0800

    Fix back navigation on orders tabs

--- app/routes/orders/client_summary.js
+++ app/routes/orders/client_summary.js
@@ -1,10 +1,3 @@
-import detail from './detail';
+import detail from "./detail";
 
-export default detail.extend({
-  setupController(controller, model){
-    if(controller) {
-      this._super(controller, model);
-      controller.set('isActiveSummary', true);
-    }
-  }
-});
+export default detail.extend({});
