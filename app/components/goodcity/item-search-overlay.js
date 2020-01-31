9b243247590da5487a3389b004244253f7df3644
akshay <akshay@kiprosh.com>
2020-01-31 19:33:00 +0530

    GCW-2859 fetch assosicated pacckage types

--- app/components/goodcity/item-search-overlay.js
+++ app/components/goodcity/item-search-overlay.js
@@ -16,6 +16,42 @@ export default Ember.Component.extend(SearchMixin, {
 
   noInput: Ember.computed.not("searchText"),
 
+  defaultChildPackagesList: function() {
+    return this._getPackages(
+      this,
+      this.get("entity")
+        .get("code")
+        .get("defaultChildPackages")
+    );
+  },
+
+  otherChildPackagesList: function() {
+    return this._getPackages(
+      this,
+      this.get("entity")
+        .get("code")
+        .get("otherChildPackages")
+    );
+  },
+
+  allChildPackagesList: function() {
+    return this.defaultChildPackagesList().concat(
+      this.otherChildPackagesList()
+    );
+  },
+
+  _getPackages: function(model, packageNames) {
+    var array = (packageNames || "").split(",");
+    var packages = [];
+    var allPackageTypes = this.get("store").peekAll("code");
+    array.forEach(function(type) {
+      allPackageTypes.filter(function(pkg) {
+        return pkg.get("code") === type ? packages.push(pkg) : "";
+      });
+    });
+    return packages;
+  },
+
   actions: {
     cancel() {
       this.set("searchText", "");
@@ -28,11 +64,13 @@ export default Ember.Component.extend(SearchMixin, {
     },
 
     loadMoreItems(pageNo) {
+      console.log(this.allChildPackagesList(), "hit");
       const params = this.trimQuery(
         _.merge({}, this.getSearchQuery(true), this.getPaginationQuery(pageNo))
       );
-
-      return this.get("store").query("item", params);
+      let items = this.get("store").query("item", params);
+      console.log(items);
+      return items;
     }
   }
 });
