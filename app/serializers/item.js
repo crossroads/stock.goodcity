ce69eeea4d97db858e8fe81a19700fe08901a19a
AbulAsar Sayyad <abul.a@kiprosh.com>
2020-02-19 18:07:28 +0530

    fixed failing specs

--- app/serializers/item.js
+++ app/serializers/item.js
@@ -1,11 +1,14 @@
 import applicationSerializer from "./application";
 
+// @TODO: delete this serializer after renaming item to package
 export default applicationSerializer.extend({
   normalizeResponse(store, primaryModelClass, payload, id, requestType) {
-    payload.offers_packages.map(op => {
-      op.item_id = op.package_id;
-      delete op.package_id;
-    });
+    if (payload.offers_packages) {
+      payload.offers_packages.map(op => {
+        op.item_id = op.package_id;
+        delete op.package_id;
+      });
+    }
     return this._super(...arguments);
   }
 });
