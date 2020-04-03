8dcf2bde03388ea5eb5850767e935a25fd00b376
AbulAsar Sayyad <abul.a@kiprosh.com>
2020-04-04 03:09:39 +0530

    loaded beneficiary in client_summary route

--- app/routes/orders/client_summary.js
+++ app/routes/orders/client_summary.js
@@ -1,3 +1,17 @@
 import detail from "./detail";
 
-export default detail.extend({});
+export default detail.extend({
+  async model() {
+    const order = await this._super(...arguments);
+    return Ember.RSVP.hash({
+      order,
+      beneficiary: this.loadIfAbsent("beneficiary", order.get("beneficiaryId"))
+    });
+  },
+
+  setupController(controller, model) {
+    if (controller) {
+      controller.set("model", model.order);
+    }
+  }
+});
