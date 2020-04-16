f20d82cc97f7323905dc77ac670858698277b1cd
bharat619 <bharat.h@kiprosh.com>
2020-04-16 11:20:24 +0530

    GCW-3122 fix if there is no beneficiary id for the order

--- app/routes/orders/client_summary.js
+++ app/routes/orders/client_summary.js
@@ -3,10 +3,19 @@ import detail from "./detail";
 export default detail.extend({
   async model() {
     const order = await this._super(...arguments);
-    return Ember.RSVP.hash({
-      order,
-      beneficiary: this.loadIfAbsent("beneficiary", order.get("beneficiaryId"))
-    });
+    if (order.get("beneficiaryId")) {
+      return Ember.RSVP.hash({
+        order,
+        beneficiary: this.loadIfAbsent(
+          "beneficiary",
+          order.get("beneficiaryId")
+        )
+      });
+    } else {
+      return Ember.RSVP.hash({
+        order
+      });
+    }
   },
 
   setupController(controller, model) {
