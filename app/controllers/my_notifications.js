2a99b82035ac0eb2928d1d67562bc592bea467fc
swatijadhav <swati.nakshtra17@gmail.com>
2020-04-24 09:20:13 +0530

    GCW-3120 fixed duplicate thread for new message

--- app/controllers/my_notifications.js
+++ app/controllers/my_notifications.js
@@ -35,12 +35,12 @@ export default Ember.Controller.extend({
     const orderId = notification.record.order_id;
     const notifications = this.get("notifications");
 
-    if (!orderId) {
-      return;
-    }
-
     if (msg.get("isUnread")) {
       this.get("messagesUtil")._incrementCount();
+    }
+
+    if (!orderId || this.session.router.currentURL !== "/my_notifications") {
+      return;
     }
 
     this.loadIfAbsent("designation", orderId).then(() => {
