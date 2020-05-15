c102c01e3f502d3cb1dc8f461c1f335fa0317825
Patrick R <patrick.rabier@gmail.com>
2020-05-15 09:55:09 +0800

    small cleanup

--- app/mixins/item_actions.js
+++ app/mixins/item_actions.js
@@ -27,8 +27,7 @@ export default Ember.Mixin.create(AsyncMixin, {
   i18n: Ember.inject.service(),
 
   itemActions: Ember.computed(function() {
-    let actionsList = ITEM_ACTIONS;
-    actionsList.map(action => {
+    return ITEM_ACTIONS.map(action => {
       return {
         ...action,
         displayName: this.get("i18n").t(
@@ -36,19 +35,14 @@ export default Ember.Mixin.create(AsyncMixin, {
         ).string
       };
     });
-    return actionsList;
   }),
 
   verifyGainAction(actionName) {
-    let currentAction = _.find(
-      this.get("itemActions"),
-      action => action.name === actionName
-    );
+    let currentAction = _.find(this.get("itemActions"), ["name", actionName]);
     return currentAction && !currentAction.loss;
   },
 
   isValidQuantity: Ember.computed("actionQty", "isGainAction", function() {
-    let action = this.get("actionName");
     let isGainAction = this.get("isGainAction");
     let value = this.get("actionQty");
     return (
