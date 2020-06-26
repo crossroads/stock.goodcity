bd80071a69a37df428c3c1d994d9f5115d922082
AbulAsar Sayyad <abul.a@kiprosh.com>
2020-06-26 19:48:21 +0530

    added groupBy logic for item_actions

--- app/controllers/items/history.js
+++ app/controllers/items/history.js
@@ -7,6 +7,16 @@ export default detail.extend(SearchMixin, {
   autoLoad: true,
   perPage: 25,
 
+  getObjectPosition(objectToBeSearched, searchElement) {
+    let position;
+    Object.keys(objectToBeSearched).map(key => {
+      if (objectToBeSearched[key].includes(searchElement)) {
+        position = key;
+      }
+    });
+    return position;
+  },
+
   actions: {
     loadMoreActions(pageNo) {
       const params = this.trimQuery(
@@ -15,7 +25,34 @@ export default detail.extend(SearchMixin, {
           this.getPaginationQuery(pageNo)
         )
       );
-      return this.get("store").query("item_action", params);
+      return this.get("store")
+        .query("item_action", params)
+        .then(items => {
+          if (!items.get("length")) {
+            return [];
+          }
+          let groupedUserItems = [];
+          let actionsAndVersions = [...items.toArray()];
+          let sortedItems = actionsAndVersions.sortBy("createdAt");
+
+          sortedItems.map((item, index) => {
+            let previousItem = sortedItems[index - 1];
+            if (
+              item.get("user.fullName").toString() ==
+              (previousItem && previousItem.get("user.fullName").toString())
+            ) {
+              const existingKey = this.getObjectPosition(
+                groupedUserItems,
+                previousItem
+              );
+              groupedUserItems[existingKey].push(item);
+            } else {
+              const newKey = `${item.get("user.fullName")}_${index}`;
+              groupedUserItems[newKey] = [item];
+            }
+          });
+          return [groupedUserItems];
+        });
     }
   }
 });
