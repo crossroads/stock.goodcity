42d9905d585442d756ed3c37f76256db16012407
AbulAsar Sayyad <abul.a@kiprosh.com>
2020-07-02 02:42:40 +0530

    added class based helpers for format-display-value

--- app/helpers/format-display-value.js
+++ app/helpers/format-display-value.js
@@ -1,11 +1,23 @@
 import Ember from "ember";
 
-export default Ember.Helper.helper(function(data) {
-  const [type, value] = data;
-  switch (type) {
-    case "expiry_date":
-      return moment(value).format("LLL");
-    default:
-      return value;
+export default Ember.Helper.extend({
+  store: Ember.inject.service(),
+
+  compute(data) {
+    const [type, value] = data;
+    switch (type) {
+      case "expiry_date":
+        return moment(value).format("LLL");
+      case "location_id":
+        return this.get("store")
+          .peekRecord("location", value || 21)
+          .getWithDefault("name", "N/A");
+      case "restriction_id":
+        return this.get("store")
+          .peekRecord("restriction", value)
+          .getWithDefault("name", "N/A");
+      default:
+        return value;
+    }
   }
 });
