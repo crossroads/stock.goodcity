7983b79a46556ebf39f6131161e6ef40f9bc1abd
AbulAsar Sayyad <abul.a@kiprosh.com>
2020-07-09 19:48:25 +0530

    added query for orders on organisation

--- app/controllers/organisations/orders.js
+++ app/controllers/organisations/orders.js
@@ -1,6 +1,30 @@
 import Ember from "ember";
+import _ from "lodash";
+import SearchMixin from "stock/mixins/search_resource";
 
-export default Ember.Controller.extend({
+export default Ember.Controller.extend(SearchMixin, {
   gcOrganisationUsers: null,
-  filteredResults: Ember.computed.alias('model.designations')
+
+  actions: {
+    /**
+     * Load the next page of the list
+     *
+     * @memberof OrdersSearchController
+     * @param {number} pageNo the page to load
+     * @returns {Promise<Order[]>}
+     */
+    loadOrders(pageNo) {
+      const cache = this.get("cache");
+      const params = this.trimQuery(
+        _.merge(
+          {
+            slug: "organisation_orders",
+            organisationId: this.get("model.id")
+          },
+          this.getPaginationQuery(pageNo)
+        )
+      );
+      return this.get("store").query("gcOrganisation", params);
+    }
+  }
 });
