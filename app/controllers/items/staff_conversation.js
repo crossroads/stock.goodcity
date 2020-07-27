63ebd2bf74b4514e83a8be625cb62de220b0679a
swatijadhav <swati.nakshtra17@gmail.com>
2020-07-27 17:57:46 +0530

    GCW-3169 addressed PR comment

--- app/controllers/items/staff_conversation.js
+++ app/controllers/items/staff_conversation.js
@@ -1,18 +1,40 @@
 import Ember from "ember";
 import MessageBase from "stock/mixins/messages_base";
+import AsyncMixin, { ERROR_STRATEGIES } from "stock/mixins/async";
 
-export default Ember.Controller.extend(MessageBase, {
+export default Ember.Controller.extend(MessageBase, AsyncMixin, {
   isPrivate: true,
 
   canManageItemMessages: Ember.computed.alias(
     "session.currentUser.canManageItemMessages"
   ),
 
+  on() {
+    if (this.get("canManageItemMessages")) {
+      this.get("subscription").on(
+        "change:message",
+        this,
+        this.markReadAndScroll
+      );
+    }
+  },
+
+  off() {
+    if (this.get("canManageItemMessages")) {
+      this.get("subscription").off(
+        "change:message",
+        this,
+        this.markReadAndScroll
+      );
+    }
+  },
+
   actions: {
     sendMessage() {
       Ember.$("textarea").trigger("blur");
       let values = this.prepareMessageObject("Package");
-      this.createMessage(values);
+
+      this.runTask(this.createMessage(values), ERROR_STRATEGIES.MODAL);
     }
   }
 });
--- app/routes/items/staff_conversation.js
+++ app/routes/items/staff_conversation.js
@@ -23,16 +23,14 @@ export default AuthorizeRoute.extend({
     this._super(controller, model);
     controller.set("messages", model.messages);
     controller.set("model", model.item);
+    controller.on();
 
     if (this.get("canManageItemMessages")) {
       controller.send("markRead");
-      controller.on();
     }
   },
 
   resetController(controller) {
-    if (this.get("canManageItemMessages")) {
-      controller.off();
-    }
+    controller.off();
   }
 });
