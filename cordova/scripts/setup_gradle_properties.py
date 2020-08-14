02e2578cb6ef848b62385ca56debd73a1b493c7e
Patrick R <patrick.rabier@gmail.com>
2020-08-14 14:49:23 +0800

    TEST: Android build

--- cordova/scripts/setup_gradle_properties.py
+++ cordova/scripts/setup_gradle_properties.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python
 
-print "This is a test"
+import os
+import sys
+
+PROPS = {
+  "android.useAndroidX": "true",
+  "android.enableJetifier": "true"
+};
+
+cordova_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
+gradle_prop_path = os.path.abspath(os.path.join(cordova_path, '../platforms/android/gradle.properties'))
+
+content = open(gradle_prop_path, "r").read()
+
+print(content);
