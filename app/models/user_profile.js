c6ca92f0d2dd8c8e459253b61d0099e57c75defc
swatijadhav <swati.nakshtra17@gmail.com>
2020-08-24 10:27:57 +0530

    GCW-3279 hide user-create link when user dont have manage-user permission

--- app/models/user_profile.js
+++ app/models/user_profile.js
@@ -67,6 +67,15 @@ export default Addressable.extend({
     );
   }),
 
+  canCreateUsers: Ember.computed("roles", function() {
+    const roles = this.get("roles");
+    return roles.find(
+      r =>
+        r.get("permissionNames").indexOf("can_read_or_modify_user") >= 0 ||
+        r.get("permissionNames").indexOf("can_create_donor") >= 0
+    );
+  }),
+
   canManageUserRoles: Ember.computed("roles", function() {
     const roles = this.get("roles");
     return roles.find(
