8876c0e753ec5be87068f093e56e5c83c6f178bf
bharat619 <bharat.h@kiprosh.com>
2020-09-16 09:27:35 +0530

    :bug: Fix regex and remove ids

--- app/constants/regex.js
+++ app/constants/regex.js
@@ -3,5 +3,5 @@ export const regex = {
   INT_REGEX: /^\d+$/,
   NON_DIGIT_REGEX: /[\D]/g,
   NON_DIGIT_FLOAT_REGEX: /[^\d\.]/g,
-  WEBSITE_REGEX: /^(www\.|https?:\/\/(www\.)?)[a-zA-Z0-9-]+\.[a-zA-Z]+\.?[a-zA-Z0-9-#.]*[a-z]$/
+  WEBSITE_REGEX: /^(www\.|https?:\/\/(www\.)?)[a-zA-Z0-9-@]+\.[a-zA-Z]+\.?[a-zA-Z0-9-#.]*[a-z]$/
 };
