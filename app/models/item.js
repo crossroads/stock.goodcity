c981fcf056c993acfea2fc6e23eee73c1d7dcbf8
bharat619 <bharat.h@kiprosh.com>
2020-10-28 14:36:42 +0530

    :lipstick: Adds translation for package notes and suggestion in create and edit screen

--- app/models/item.js
+++ app/models/item.js
@@ -41,6 +41,7 @@ export default cloudinaryUrl.extend(GradeMixin, {
   onHandBoxedQuantity: attr("number"),
   onHandPalletizedQuantity: attr("number"),
   notes: attr("string"),
+  notesZhTw: attr("string"),
   restrictionId: attr("number"),
   // Temporarily keep the old `quantity` field as an alias to make migration easier
   quantity: Ember.computed.alias("availableQuantity"),
