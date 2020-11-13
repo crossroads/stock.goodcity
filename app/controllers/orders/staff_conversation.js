76c1351672aea4b8296d5ba7b894710a1621ace5
shreyas098 <shreyash@kiprosh.com>
2020-11-13 13:36:58 +0530

    GCW-3236 QA fix for back button issue while creating international orders and importing lodash in staff conversation file

--- app/controllers/orders/staff_conversation.js
+++ app/controllers/orders/staff_conversation.js
@@ -1,4 +1,5 @@
 import Ember from "ember";
+import _ from "lodash";
 
 import conversation from "./conversation";
 import { ROLES } from "stock/constants/roles";
@@ -6,6 +7,6 @@ import { ROLES } from "stock/constants/roles";
 export default conversation.extend({
   isPrivate: true,
   roles: Ember.computed(function() {
-    return _.values(ROLES["STOCK_APP_ROLES"])
+    return _.values(ROLES["STOCK_APP_ROLES"]);
   })
 });
