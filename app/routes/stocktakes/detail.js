d6fc75e88efcf5858803a1872a7c0fe83d72d8c6
Patrick R <patrick.rabier@gmail.com>
2021-01-13 10:36:19 +0800

    :zap: GCW-3444 UPDATE: Stocktake shallow loading for speed improvements

--- app/routes/stocktakes/detail.js
+++ app/routes/stocktakes/detail.js
@@ -4,7 +4,9 @@ import Ember from "ember";
 export default AuthorizeRoute.extend({
   model({ stocktake_id }) {
     return Ember.RSVP.hash({
-      stocktake: this.store.findRecord("stocktake", stocktake_id)
+      stocktake: this.store.findRecord("stocktake", stocktake_id, {
+        reload: true
+      })
     });
   },
 
