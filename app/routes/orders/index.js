c6557e9e6090fcadafa14dc3be486a711a97ce21
bharat619 <bharat.h@kiprosh.com>
2021-02-10 11:40:52 +0530

    :wrench: Fix wrong language headers sent

--- app/routes/orders/index.js
+++ app/routes/orders/index.js
@@ -7,17 +7,15 @@ import { STATE_FILTERS } from "../../services/filter-service";
 export default AuthorizeRoute.extend({
   filterService: Ember.inject.service(),
   utilityMethods: Ember.inject.service(),
+  userService: Ember.inject.service(),
 
   /* jshint ignore:start */
   async model(params, transition) {
     if (!this.session.get("currentUser")) {
       // @TODO: Move this user api call into the session service
       // Checking the user, if needed, should probably be in AuthorizeRoute
-      let data = await new AjaxPromise(
-        "/auth/current_user_profile",
-        "GET",
-        this.session.get("authToken")
-      );
+      let data = await this.get("userService").currentUser();
+
       this.store.pushPayload(data);
     }
   },
