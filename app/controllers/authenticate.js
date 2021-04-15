93172baaf506a8138e5136ce1e35fff89f1c6c87
shreyas098 <shreyash@kiprosh.com>
2021-04-16 03:24:04 +0530

    Added changes for templates of timer function

--- app/controllers/authenticate.js
+++ app/controllers/authenticate.js
@@ -11,11 +11,27 @@ export default GoodcityController.extend(preloadDataMixin, {
   authService: Ember.inject.service(),
   attemptedTransition: null,
   pin: "",
+  otpResendTime: config.APP.OTP_RESEND_TIME,
+  pinAlreadySent: false,
   isMobileApp: config.cordova.enabled,
 
   mobile: Ember.computed("mobilePhone", function() {
     return config.APP.HK_COUNTRY_CODE + this.get("mobilePhone");
   }),
+
+  timerFunction() {
+    let waitTime = this.get("otpResendTime");
+    if (waitTime == 0) {
+      this.set("pinAlreadySent", false);
+      this.set("otpResendTime", config.APP.OTP_RESEND_TIME);
+      return false;
+    }
+    this.set("timer", waitTime);
+    this.set("otpResendTime", waitTime - 1);
+    return setTimeout(() => {
+      this.timerFunction();
+    }, 1000);
+  },
 
   actions: {
     authenticateUser() {
@@ -26,6 +42,7 @@ export default GoodcityController.extend(preloadDataMixin, {
       this.get("authService")
         .verify(pin, otpAuthKey)
         .then(({ jwt_token, user }) => {
+          this.set("otpResendTime", 0);
           this.set("pin", null);
           this.set("session.authToken", jwt_token);
           this.set("session.otpAuthKey", null);
@@ -61,6 +78,8 @@ export default GoodcityController.extend(preloadDataMixin, {
           this.set("session.otpAuthKey", data.otp_auth_key);
           this.set("pin", null);
           this.transitionToRoute("/authenticate");
+          this.set("pinAlreadySent", true);
+          this.timerFunction();
         })
         .catch(error => {
           if ([401].includes(error.status)) {
