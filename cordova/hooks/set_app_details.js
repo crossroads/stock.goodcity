dd7431c96234bb29accccb4f770c085d60a5b468
Steve Kenworthy <steveyken@gmail.com>
2021-12-04 11:45:11 +0800

    Need to change app id before cordova runs.

--- cordova/hooks/set_app_details.js
+++ cordova/hooks/set_app_details.js
@@ -11,18 +11,18 @@
 //   process.env.CIRCLE_BUILD_NUM = <numeric>
 
 module.exports = context => {
-  var android_build_version_seed = 8000;
-  var androidversionCode = "";
-  var iosCFBundleVersion = "";
+  let android_build_version_seed = 80000;
+  let androidversionCode = "";
+  let iosCFBundleVersion = "";
 
   const cordovaCommon = context.requireCordovaModule("cordova-common");
 
   // staging or production?
-  var staging = process.env.ENVIRONMENT !== "production";
+  let staging = process.env.ENVIRONMENT !== "production";
 
   // append CIRCLE_BUILD_NUM to app version
-  var path = require("path");
-  var pkg = require("../../package.json");
+  let path = require("path");
+  let pkg = require("../../package.json");
   app_version = pkg.version;
   build_num = parseInt(process.env.CIRCLE_BUILD_NUM);
   if (!isNaN(build_num)) {
@@ -36,42 +36,13 @@ module.exports = context => {
     iosCFBundleVersion = circle_build_num;
   }
 
-  // Update package.json
-  const fs = require("fs");
-  const filename = path.join(context.opts.projectRoot, "package.json");
-  const file = require(filename);
-  var app_name = file.name;
-  var displayName = file.displayName;
-  if (staging) {
-    if (!app_name.endsWith("staging")) {
-      app_name = app_name + "staging";
-    }
-    if (!displayName.startsWith("S. ")) {
-      displayName = "S. " + displayName;
-    }
-  } else {
-    app_name = app_name.replace(/staging/, "");
-    displayName = displayName.replace(/S\. /, "");
-  }
-  file.name = app_name;
-  file.version = app_version;
-  file.displayName = displayName;
-  fs.writeFileSync(filename, JSON.stringify(file, null, 2));
-
   //config.xml
-  var configPath = path.join(context.opts.projectRoot, "config.xml");
-  var config = new cordovaCommon.ConfigParser(configPath);
+  let configPath = path.join(context.opts.projectRoot, "config.xml");
+  let config = new cordovaCommon.ConfigParser(configPath);
   config.setPackageName(app_name);
   config.setName(displayName);
   config.setVersion(app_version);
   config.doc.getroot().set("android-versionCode", androidversionCode);
   config.doc.getroot().set("ios-CFBundleVersion", iosCFBundleVersion);
   config.write();
-
-  // output changes
-  console.log("Set app id: " + app_name);
-  console.log("Set app name: " + displayName);
-  console.log("Set app version: " + app_version);
-  console.log("Set Android version code: " + androidversionCode);
-  console.log("Set iOS bundle version: " + iosCFBundleVersion);
 };
