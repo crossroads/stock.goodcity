63fe683f7089d2f057b658a89b2cd61e5d12d6c2
Steve <steveyken@gmail.com>
2025-12-11 14:13:57 +0800

    Allow search for single chinese characters.

--- app/controllers/manage_users.js
+++ app/controllers/manage_users.js
@@ -1,14 +1,28 @@
 import Ember from "ember";
 
 export default Ember.Controller.extend({
-  minSearchTextLength: 1,
+  // Minimum length for non-Chinese/Japanese/Korean (CJK) searches
+  minSearchTextLength: 3,
   displayResults: false,
-
+  isSearchTextLongEnough(text) {
+    // length >= 1 for CJK, otherwise length >= minSearchTextLength
+    if (!text) return false;
+    const trimmed = text.trim();
+    if (!trimmed) return false;
+    const length = Array.from(trimmed).length;
+    const minSearchTextLength = this.get("minSearchTextLength");
+    const isCJKPresent = /[\u3400-\u4DBF\u4E00-\u9FFF\uF900-\uFAFF]/.test(
+      trimmed
+    );
+    if (isCJKPresent) {
+      return length >= 1;
+    } else {
+      return length >= minSearchTextLength;
+    }
+  },
   onSearchTextChange: Ember.observer("searchText", function() {
     this.hideResults();
-    if (
-      this.get("searchText").trim().length >= this.get("minSearchTextLength")
-    ) {
+    if (this.isSearchTextLongEnough(this.get("searchText"))) {
       Ember.run.debounce(this, this.showResults, 500);
     }
   }),
